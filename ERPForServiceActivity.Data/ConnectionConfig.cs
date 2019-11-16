c523aeb60afdf51f0b25e86e3e51169e92105a49
ALMLAMBO <aleksm007148@gmail.com>
2019-11-16 15:23:00 +0200

    Add class for configuring connection to the firebase

---
+++ ERPForServiceActivity.Data/ConnectionConfig.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Google.Cloud.Firestore;
+
+namespace ERPForServiceActivity.Data {
+	class ConnectionConfig {
+		private FirestoreDb database;
+		private string projectId;
+
+		private void SetUp() {
+			string filepath = @"E:\\Diploma-project\\ERP-for-service-activity\\ERPForServiceActivity.Data\\erp-for-service-activity-d0a7eb625242.json";
+			Environment.SetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS", filepath);
+			projectId = "erp-for-service-activity";
+			database = FirestoreDb.Create(projectId);
+		}
+		
+		public FirestoreDb GetFirestoreDb() {
+			SetUp();
+
+			return database;
+		}
+	}
+}
