2f86c0f03afb96a3b98000cebf99808106308d30
ALMLAMBO <aleksm007148@gmail.com>
2019-11-16 16:26:06 +0200

    Add models for uploading licenses and for license key

---
+++ ERPForServiceActivity.CommonModels/BindingModels/Licenses/AddLicenseBindingModel.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using Google.Cloud.Firestore;
+
+namespace ERPForServiceActivity.CommonModels.BindingModels.Licenses {
+	[FirestoreData]
+	public class AddLicenseBindingModel {
+		public string Id { get; set; }
+		
+		[FirestoreProperty]
+		[Required]
+		[DataType(DataType.Text)]
+		public string ServiceName { get; set; }
+
+		[FirestoreProperty]
+		public List<LicenseKeyBindingModel> LicenseKeys { get; set; }
+	}
+}
---
+++ ERPForServiceActivity.CommonModels/BindingModels/Licenses/LicenseKeyBindingModel.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ERPForServiceActivity.CommonModels.BindingModels.Licenses {
+	public class LicenseKeyBindingModel {
+		public string LicenseKey { get; set; }
+	}
+}
