365ebdd432846dd879dbee0a7e166486f5b9dbca
ALMLAMBO <aleksm007148@gmail.com>
2019-11-17 23:41:32 +0200

    Add test for testing spesific collection and fix simple connection test

--- ERPForServiceActivity.Tests/FirebaseConnectionTest.cs
+++ ERPForServiceActivity.Tests/FirebaseConnectionTest.cs
@@ -15,14 +15,43 @@ namespace ERPForServiceActivity.Tests {
 			Assert.NotEqual(0, qs.Count);
 
 			Query query1 = database.Collection("licenses");
-			QuerySnapshot qs1 = await query.GetSnapshotAsync();
+			QuerySnapshot qs1 = await query1.GetSnapshotAsync();
 
 			Assert.NotEqual(0, qs1.Count);
 
-			Query query2 = database.Collection("repairs");
-			QuerySnapshot qs2 = await query.GetSnapshotAsync();
+			Query query2 = database.Collection("service-repairs");
+			QuerySnapshot qs2 = await query2.GetSnapshotAsync();
 
 			Assert.NotEqual(0, qs2.Count);
+		}
+
+		[Fact]
+		public async void CheckServiceRepairsNestedCollections() {
+			ConnectionConfig config = new ConnectionConfig();
+			FirestoreDb database = config.GetFirestoreDb();
+			
+			Query q1 = database.Collection("service-repairs");
+			QuerySnapshot qs1 = await q1.GetSnapshotAsync();
+
+			Assert.NotNull(qs1);
+
+			Query q2 = database.Collection("service-repairs")
+				.Document("kk5M3Pl4Zvme6db7kn7p")
+				.Collection("repairs");
+
+			QuerySnapshot qs2 = await q2.GetSnapshotAsync();
+
+			Assert.NotNull(qs2);
+
+			Query q3 = database.Collection("service-repairs")
+				.Document("kk5M3Pl4Zvme6db7kn7p")
+				.Collection("repairs")
+				.Document("9iXz590oBLwVGhjCYr1K")
+				.Collection("parts");
+
+			QuerySnapshot qs3 = await q3.GetSnapshotAsync();
+
+			Assert.NotNull(qs3);
 		}
 	}
 }
