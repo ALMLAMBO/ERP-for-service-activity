be6d9d0f7ee72593b6ee3b8fa92e3c606301f86f
ALMLAMBO <aleksm007148@gmail.com>
2019-11-21 21:58:13 +0200

    Add validations for service name and license key

--- ERPForServiceActivity.CommonModels/BindingModels/Licenses/AddAuthorizedServiceBindingModel.cs
+++ ERPForServiceActivity.CommonModels/BindingModels/Licenses/AddAuthorizedServiceBindingModel.cs
@@ -1,22 +1,29 @@
-﻿using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
 using Google.Cloud.Firestore;
+using ERPForServiceActivity.Common;
+using ERPForServiceActivity.Security;
 
 namespace ERPForServiceActivity.CommonModels.BindingModels.Licenses {
 	[FirestoreData]
 	public class AddAuthorizedServiceBindingModel {
 		public string Id { get; set; }
 		
-		[FirestoreProperty]
 		[Required]
+		[FirestoreProperty]
 		[DataType(DataType.Text)]
+		[MinLength(CommonConstants.MinLengthServiceName, 
+			ErrorMessage = CommonConstants.ErrorMessageWrongLength)]
+
 		public string AuthorizedService { get; set; }
 
 		[FirestoreProperty]
 		public string UserId { get; set; }
 
-		[FirestoreProperty]
 		[Required]
+		[FirestoreProperty]
+		[StringLength(CommonSecurityConstants.LicenseLength, 
+			ErrorMessage = CommonConstants.ErrorMessageIncorrectLicense)]
+
 		public string LicenseKey { get; set; }
 	}
 }
