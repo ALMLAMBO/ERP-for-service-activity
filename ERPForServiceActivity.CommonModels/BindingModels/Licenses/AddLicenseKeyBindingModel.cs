2917742885a2fcc0e855e91099d8ddf37a397636
ALMLAMBO <aleksm007148@gmail.com>
2019-11-21 21:40:50 +0200

    Rename class for normal users keys and add property for user id

---
+++ ERPForServiceActivity.CommonModels/BindingModels/Licenses/AddLicenseKeyBindingModel.cs
@@ -0,0 +1,15 @@
+﻿using Google.Cloud.Firestore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ERPForServiceActivity.CommonModels.BindingModels.Licenses {
+	[FirestoreData]
+	public class AddLicenseKeyBindingModel {
+		[FirestoreProperty]
+		public string UserId { get; set; }
+
+		[FirestoreProperty]
+		public string LicenseKey { get; set; }
+	}
+}
--- ERPForServiceActivity.CommonModels/BindingModels/Licenses/LicenseKeyBindingModel.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace ERPForServiceActivity.CommonModels.BindingModels.Licenses {
-	public class LicenseKeyBindingModel {
-		public string LicenseKey { get; set; }
-	}
-}
