bffbdaa4a864c6ad83f6e8adace214c9fcfe6c76
ALMLAMBO <aleksm007148@gmail.com>
2019-11-21 21:59:35 +0200

    Add validations for license key and user id

--- ERPForServiceActivity.CommonModels/BindingModels/Licenses/AddLicenseKeyBindingModel.cs
+++ ERPForServiceActivity.CommonModels/BindingModels/Licenses/AddLicenseKeyBindingModel.cs
@@ -1,15 +1,19 @@
-﻿using Google.Cloud.Firestore;
-using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.ComponentModel.DataAnnotations;
+using Google.Cloud.Firestore;
+using ERPForServiceActivity.Common;
+using ERPForServiceActivity.Security;
 
 namespace ERPForServiceActivity.CommonModels.BindingModels.Licenses {
 	[FirestoreData]
 	public class AddLicenseKeyBindingModel {
+		[Required]
 		[FirestoreProperty]
 		public string UserId { get; set; }
 
 		[FirestoreProperty]
+		[StringLength(CommonSecurityConstants.LicenseLength,
+			ErrorMessage = CommonConstants.ErrorMessageIncorrectLicense)]
+
 		public string LicenseKey { get; set; }
 	}
 }
