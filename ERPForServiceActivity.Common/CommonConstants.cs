25c56f37d919bdc0ecc5a4a1a3fb1035a95383d7
ALMLAMBO <aleksm007148@gmail.com>
2019-11-21 21:56:31 +0200

    Rename class1 to common constants and add some constants

---
+++ ERPForServiceActivity.Common/CommonConstants.cs
@@ -0,0 +1,8 @@
+﻿
+namespace ERPForServiceActivity.Common {
+	public class CommonConstants {
+		public const int MinLengthServiceName = 3;
+		public const string ErrorMessageWrongLength = "Enter at least 3 symbols";
+		public const string ErrorMessageIncorrectLicense = "You entered incorrect license";
+	}
+}
