e35cf60a043cfa59ff2491b04797870dc4b42d2f
ALMLAMBO <aleksm007148@gmail.com>
2019-11-24 11:57:42 +0200

    Add class for testing getting data from image

---
+++ ERPForServiceActivity.Tests/GetDataFromImageTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Google.Cloud.Vision.V1;
+using Xunit;
+using ERPForServiceActivity.DataFromImage;
+
+namespace ERPForServiceActivity.Tests {
+	public class GetDataFromImageTest {
+		[Fact]
+		public void TestGetSerialNumber() {
+			GetDataFromImage data = new GetDataFromImage();
+			string sn = data.GetSerialNumber();
+			
+			Assert.NotNull(sn);
+			Assert.Equal("803MAPN5L200", sn);
+		}
+	}
+}
