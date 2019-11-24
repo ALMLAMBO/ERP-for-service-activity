9c41f1f8f1c27da8a157c9f75d33ec6cb8e0958d
ALMLAMBO <aleksm007148@gmail.com>
2019-11-24 11:41:20 +0200

    Add method for getting serial number

--- ERPForServiceActivity.DataFromImage/DataFromImage.cs
+++ ERPForServiceActivity.DataFromImage/DataFromImage.cs
@@ -1,9 +1,41 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
+using System.Text.RegularExpressions;
+using ERPForServiceActivity.Security;
+using Google.Cloud.Vision.V1;
 
 namespace ERPForServiceActivity.DataFromImage {
 	public class DataFromImage {
-		
+		private string filepath = @"E:\Diploma-project\ERP-for-service-activity\ERPForServiceActivity.DataFromImage\erp-for-service-activity-7896e2fd1fc6.json";
+		private ImageAnnotatorClient client;
+
+		public DataFromImage() {
+			Environment.SetEnvironmentVariable(
+				"GOOGLE_APPLICATION_CREDENTIALS", filepath);
+
+			client = ImageAnnotatorClient.Create();
+		}
+
+		private IReadOnlyList<EntityAnnotation> GetImageData() {
+			Image image = Image.FromFile(@"E:\ALEKS\test_photo4.jpg");
+
+			return client.DetectText(image);
+		}
+
+		public string GetSerialNumber() {
+			var response = GetImageData();
+			string serialNumber = "";
+
+			foreach (var annotation in response) {
+				Match snMatch = CommonSecurityConstants
+					.getLGSerialNumber.Match(annotation.Description);
+
+				if(snMatch.Success && !string.IsNullOrEmpty(serialNumber)) {
+					serialNumber = annotation.Description;
+				}
+			}
+
+			return serialNumber;
+		}
 	}
 }
