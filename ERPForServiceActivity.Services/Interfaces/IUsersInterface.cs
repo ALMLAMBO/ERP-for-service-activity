294029089cf4b0b0a871e8faf7212857231f6bec
ALMLAMBO <aleksm007148@gmail.com>
2019-11-28 00:18:48 +0200

    Add interface for managing users

---
+++ ERPForServiceActivity.Services/Interfaces/IUsersInterface.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading.Tasks;
+using ERPForServiceActivity.Models.User;
+using FirebaseAdmin.Auth;
+
+namespace ERPForServiceActivity.Services.Interfaces {
+	public interface IUsersInterface {
+		public void AddUser(string role, UserInfo userToAdd);
+
+		public Task<UserRecord> GetUserData(string uid);
+
+		public void UpdateUser(string uid, UserRecordArgs updatedUser);
+
+		public void DeleteUser(string uid);
+	}
+}
