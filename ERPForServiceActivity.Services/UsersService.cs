4bb25e6568b9b110a7b264d362e8698a18997046
ALMLAMBO <aleksm007148@gmail.com>
2019-11-28 00:19:10 +0200

    Add class which implements users interface

---
+++ ERPForServiceActivity.Services/UsersService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using FirebaseAdmin.Auth;
+using ERPForServiceActivity.Services.Interfaces;
+using ERPForServiceActivity.Models.User;
+
+namespace ERPForServiceActivity.Services {
+	public class UsersService : IUsersInterface {
+		public async void AddUser(string role, UserInfo userToAdd) {
+			//TODO IT
+			throw new NotImplementedException();
+		}
+
+		public async void DeleteUser(string uid) {
+			await FirebaseAuth.DefaultInstance.DeleteUserAsync(uid);
+		}
+
+		public async Task<UserRecord> GetUserData(string uid) {
+			return await FirebaseAuth.DefaultInstance.GetUserAsync(uid);
+		}
+
+		public async void UpdateUser(string uid, UserRecordArgs updatedUser) {
+			//TODO IT
+			throw new NotImplementedException();
+		}
+	}
+}
