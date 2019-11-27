152b03b06a95917beeb101334c967ed99adf1b48
ALMLAMBO <aleksm007148@gmail.com>
2019-11-27 23:55:59 +0200

    Add model for current service and its license key

---
+++ ERPForServiceActivity.Models/License/AuthorizedService.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+using ERPForServiceActivity.Common;
+using ERPForServiceActivity.Security;
+using Google.Cloud.Firestore;
+
+namespace ERPForServiceActivity.Models.License {
+	[FirestoreData]
+	public class AuthorizedService {
+		public AuthorizedService() {
+
+		}
+		
+		public string Id { get; set; }
+
+		[Required]
+		[FirestoreProperty]
+		[DataType(DataType.Text)]
+		[MinLength(CommonConstants.MinLengthServiceName,
+			ErrorMessage = CommonConstants.ErrorMessageWrongLength)]
+
+		public string AuthorizedServiceName { get; set; }
+
+		[Required]
+		[FirestoreProperty]
+		public string UserId { get; set; }
+
+		[Required]
+		[FirestoreProperty]
+		[StringLength(CommonSecurityConstants.LicenseLength,
+			ErrorMessage = CommonConstants.ErrorMessageIncorrectLicense)]
+
+		public string LicenseKey { get; set; }
+	}
+}
