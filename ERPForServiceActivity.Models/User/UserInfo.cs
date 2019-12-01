1616270bc9c2ceb7f5860d9708e2c21b512012a3
ALMLAMBO <aleksm007148@gmail.com>
2019-12-01 13:26:14 +0200

    Add model for the user

---
+++ ERPForServiceActivity.Models/User/UserInfo.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using ERPForServiceActivity.Common;
+
+namespace ERPForServiceActivity.Models.User {
+	public class UserInfo {
+		[Required]
+		[DataType(DataType.EmailAddress, ErrorMessage = 
+			CommonConstants.EnteredInvalidEmailAddress)]
+
+		public string Email { get; set; }
+
+		[Required]
+		public string Username { get; set; }
+
+		[Required]
+		[DataType(DataType.Password)]
+		public string Password { get; set; }
+
+		[Required]
+		public string Role { get; set; }
+	}
+}
