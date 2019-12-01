b1127e8c41d8152edc2d915b152e4e06062dcc6c
ALMLAMBO <aleksm007148@gmail.com>
2019-12-01 13:24:06 +0200

    Add view model for the user

---
+++ ERPForServiceActivity.CommonModels/ViewModels/Users/UserViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ERPForServiceActivity.CommonModels.ViewModels.Users {
+	public class UserViewModel {
+		public string Email { get; set; }
+
+		public string Username { get; set; }
+
+		public string Role { get; set; }
+	}
+}
