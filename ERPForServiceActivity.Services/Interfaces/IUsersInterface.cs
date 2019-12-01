e28266b7232448bf04c9a557af686e3c39c604d4
ALMLAMBO <aleksm007148@gmail.com>
2019-12-01 14:46:22 +0200

    Add methods for log in and log out

--- ERPForServiceActivity.Services/Interfaces/IUsersInterface.cs
+++ ERPForServiceActivity.Services/Interfaces/IUsersInterface.cs
@@ -1,6 +1,7 @@
 ﻿using System.Threading.Tasks;
 using FirebaseAdmin.Auth;
 using ERPForServiceActivity.CommonModels.BindingModels.Users;
+using ERPForServiceActivity.Models.User;
 
 namespace ERPForServiceActivity.Services.Interfaces {
 	public interface IUsersInterface {
@@ -8,6 +9,12 @@ namespace ERPForServiceActivity.Services.Interfaces {
 
 		public Task<UserRecord> GetUserData(string uid);
 
+		public void UpdateUser(UserInfo user, EditUserBindingModel editUser);
+
 		public void DeleteUser(string uid);
+
+		public Task<bool> LogIn(UserInfo user);
+
+		public void LogOut();
 	}
 }
