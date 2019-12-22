d3504ca3cfef9e164dd8c042401226ae654c1bea
ALMLAMBO <aleksm007148@gmail.com>
2019-12-22 13:12:53 +0200

    Add binding model for repair

---
+++ ERPForServiceActivity.CommonModels/BindingModels/Repairs/AddRepairBindingModel.cs
@@ -0,0 +1,42 @@
+﻿using BlazorInputFile;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace ERPForServiceActivity.CommonModels.BindingModels.Repairs {
+	public class AddRepairBindingModel {
+		[Required]
+		public int RepairId { get; set; }
+
+		public string CustomerName { get; set; }
+
+		public string CustomerAddress { get; set; }
+
+		public bool GoingToAddress { get; set; }
+
+		public string CustomerPhoneNumber { get; set; }
+
+		public bool InWarranty { get; set; }
+
+		public string ApplianceModel { get; set; }
+
+		public string ApplianceSerialNumber { get; set; }
+
+		public string ApplianceProductCodeOrImei { get; set; }
+
+		public long WarrantyCardNumber { get; set; }
+
+		[DataType(DataType.Date)]
+		public DateTime BoughtAt { get; set; }
+
+		public string Defect { get; set; }
+
+		public string AdditionalInformation { get; set; }
+
+		public IFileListEntry AppliancePhotoInfo { get; set; }
+
+		public IFileListEntry [] OtherPhotos { get; set; }
+	}
+}
