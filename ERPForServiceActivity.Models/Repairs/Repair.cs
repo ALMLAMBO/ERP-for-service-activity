23ab5964b6e274a87fe1b03b6333eee5ec82e6d1
ALMLAMBO <aleksm007148@gmail.com>
2019-12-22 15:00:27 +0200

    Add model for the repair

---
+++ ERPForServiceActivity.Models/Repairs/Repair.cs
@@ -0,0 +1,45 @@
+﻿using BlazorInputFile;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace ERPForServiceActivity.Models.Repairs {
+	public class Repair {
+		[Required]
+		public int RepairId { get; set; }
+
+		public string CustomerName { get; set; }
+
+		public string CustomerAddress { get; set; }
+
+		public bool GoingToAddress { get; set; }
+
+		public string CustomerPhoneNumber { get; set; }
+
+		public bool InWarranty { get; set; }
+
+		public string ApplicanceBrand { get; set; }
+
+		public string ApplianceType { get; set; }
+
+		public string ApplianceModel { get; set; }
+
+		public string ApplianceSerialNumber { get; set; }
+
+		public string ApplianceProductCodeOrImei { get; set; }
+
+		public long WarrantyCardNumber { get; set; }
+
+		[DataType(DataType.Date)]
+		public DateTime BoughtAt { get; set; }
+
+		public string Defect { get; set; }
+
+		public string AdditionalInformation { get; set; }
+
+		public IFileListEntry AppliancePhotoInfo { get; set; }
+
+		public IFileListEntry [] OtherPhotos { get; set; }
+	}
+}
