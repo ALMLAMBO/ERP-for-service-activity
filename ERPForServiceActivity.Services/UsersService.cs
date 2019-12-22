e789be98297778aef6c297d6c2d4ba398f9b2b71
ALMLAMBO <aleksm007148@gmail.com>
2019-12-22 13:24:03 +0200

    Add methods for getting user data, updating user, login and logout

--- ERPForServiceActivity.Services/UsersService.cs
+++ ERPForServiceActivity.Services/UsersService.cs
@@ -2,11 +2,12 @@
 using System.Threading.Tasks;
 using System.Security.Claims;
 using FirebaseAdmin.Auth;
+using FirebaseAdmin;
 using Google.Apis.Auth.OAuth2;
 using ERPForServiceActivity.Services.Interfaces;
 using ERPForServiceActivity.Security;
 using ERPForServiceActivity.CommonModels.BindingModels.Users;
-using FirebaseAdmin;
+using ERPForServiceActivity.Models.User;
 
 namespace ERPForServiceActivity.Services {
 	public class UsersService : IUsersInterface {
@@ -36,12 +37,41 @@ namespace ERPForServiceActivity.Services {
 				.SetCustomUserClaimsAsync(record.Uid, claims);
 		}
 
+		public async Task<UserRecord> GetUserData(string uid) {
+			return await FirebaseAuth.DefaultInstance.GetUserAsync(uid);
+		}
+
+		public async void UpdateUser(UserInfo user, EditUserBindingModel editUser) {
+			if(editUser != null) {
+				UserRecord userToUpdate = await FirebaseAuth.DefaultInstance
+					.GetUserByEmailAsync(user.Email);
+
+				UserRecordArgs newUserData = new UserRecordArgs() {
+					DisplayName = editUser.Username,
+					Password = editUser.Password
+				};
+
+				await FirebaseAuth.DefaultInstance.UpdateUserAsync(newUserData);
+			}
+		}
+
 		public async void DeleteUser(string uid) {
 			await FirebaseAuth.DefaultInstance.DeleteUserAsync(uid);
 		}
 
-		public async Task<UserRecord> GetUserData(string uid) {
-			return await FirebaseAuth.DefaultInstance.GetUserAsync(uid);
+		public async Task<bool> LogIn(UserInfo user) {
+			UserRecord userResponse = await FirebaseAuth
+				.DefaultInstance.GetUserByEmailAsync(user.Email);
+
+			if(userResponse == null) {
+				return false;
+			}
+
+			return true;
+		}
+
+		public async void LogOut() {
+			
 		}
 	}
 }
