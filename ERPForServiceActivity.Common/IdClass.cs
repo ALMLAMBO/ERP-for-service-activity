9ac985a887216cb7540358cd70df4ae430b3e9e5
ALMLAMBO <aleksm007148@gmail.com>
2019-12-30 20:26:36 +0200

    Add class for repairs id

---
+++ ERPForServiceActivity.Common/IdClass.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ERPForServiceActivity.Common {
+	public class IdClass {
+		public static int Id { get; set; } = 1;
+	}
+}
