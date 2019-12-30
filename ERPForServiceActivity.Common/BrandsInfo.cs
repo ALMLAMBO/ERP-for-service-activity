895914287b5cab1b5b73ca8bf7d6ededea2120a5
ALMLAMBO <aleksm007148@gmail.com>
2019-12-30 20:26:11 +0200

    Add class for brands and available models

---
+++ ERPForServiceActivity.Common/BrandsInfo.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ERPForServiceActivity.Common {
+	public class BrandsInfo {
+		public static List<string> Brands { get; set; } = new List<string>() {
+			"LG", "Samsung", "Huawei", "Arielli", "Asus"
+		};
+
+		public static Dictionary<string, List<string>>
+			ModelsForBrand { get; set; } = 
+			new Dictionary<string, List<string>>() {
+				{"LG", new List<string>() {
+					"", "", ""
+				} },
+
+				{"Samsung", new List<string>() {
+					"", "", ""
+				} },
+
+				{"Asus", new List<string>() {
+					"", "", "" 
+				} },
+
+				{"Huawei", new List<string>() { 
+					"", "", "" 
+				} },
+
+				{"Arielli", new List<string>() { 
+					"", "", ""
+				} }
+			};
+	}
+}
