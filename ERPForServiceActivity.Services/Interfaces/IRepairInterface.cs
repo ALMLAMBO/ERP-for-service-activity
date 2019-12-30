396b49ee5cd829c99115c45e59d4d3b60df40e56
ALMLAMBO <aleksm007148@gmail.com>
2019-12-30 20:27:20 +0200

    Add interface for repairs

---
+++ ERPForServiceActivity.Services/Interfaces/IRepairInterface.cs
@@ -0,0 +1,7 @@
+﻿using ERPForServiceActivity.CommonModels.BindingModels.Repairs;
+
+namespace ERPForServiceActivity.Services.Interfaces {
+	public interface IRepairInterface {
+		public void UploadRepair(string ServiceName, AddRepairBindingModel repair);
+	}
+}
