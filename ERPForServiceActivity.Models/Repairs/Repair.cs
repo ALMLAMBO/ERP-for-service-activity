1df1b04a943d4bd7bd601593850670803302dc70
ALMLAMBO <aleksm007148@gmail.com>
2019-12-30 11:38:56 +0200

    Add annotations

--- ERPForServiceActivity.Models/Repairs/Repair.cs
+++ ERPForServiceActivity.Models/Repairs/Repair.cs
@@ -1,45 +1,77 @@
-﻿using BlazorInputFile;
-using System;
-using System.Collections.Generic;
+﻿using System;
 using System.ComponentModel.DataAnnotations;
-using System.Text;
+using BlazorInputFile;
+using Google.Cloud.Firestore;
 
 namespace ERPForServiceActivity.Models.Repairs {
+	[FirestoreData]
 	public class Repair {
+		public string Id { get; set; }
+
 		[Required]
+		[FirestoreProperty]
 		public int RepairId { get; set; }
 
+		[Required]
+		[FirestoreProperty]
 		public string CustomerName { get; set; }
 
+		[Required]
+		[FirestoreProperty]
 		public string CustomerAddress { get; set; }
 
+		[Required]
+		[FirestoreProperty]
 		public bool GoingToAddress { get; set; }
 
+		[Required]
+		[FirestoreProperty]
 		public string CustomerPhoneNumber { get; set; }
 
+		[Required]
+		[FirestoreProperty]
 		public bool InWarranty { get; set; }
 
+		[Required]
+		[FirestoreProperty]
 		public string ApplianceBrand { get; set; }
 
+		[Required]
+		[FirestoreProperty]
 		public string ApplianceType { get; set; }
 
+		[Required]
+		[FirestoreProperty]
 		public string ApplianceModel { get; set; }
 
+		[Required]
+		[FirestoreProperty]
 		public string ApplianceSerialNumber { get; set; }
 
+		[Required]
+		[FirestoreProperty]
 		public string ApplianceProductCodeOrImei { get; set; }
 
+		[Required]
+		[FirestoreProperty]
 		public long WarrantyCardNumber { get; set; }
 
+		[Required]
+		[FirestoreProperty]
 		[DataType(DataType.Date)]
 		public DateTime BoughtAt { get; set; }
 
+		[Required]
+		[FirestoreProperty]
 		public string Defect { get; set; }
 
+		[FirestoreProperty]
 		public string AdditionalInformation { get; set; }
 
+		[Required]
 		public IFileListEntry AppliancePhotoInfo { get; set; }
 
+		[Required]
 		public IFileListEntry [] OtherPhotos { get; set; }
 	}
 }
