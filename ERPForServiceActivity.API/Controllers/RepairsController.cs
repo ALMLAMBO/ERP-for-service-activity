c0b975a9e262ab6e0687b5f47f28ce41c88a139b
ALMLAMBO <aleksm007148@gmail.com>
2020-01-10 23:04:04 +0200

    Add controller for handling requests for repairs

---
+++ ERPForServiceActivity.API/Controllers/RepairsController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using ERPForServiceActivity.CommonModels.BindingModels.Repairs;
+using ERPForServiceActivity.Services;
+
+namespace ERPForServiceActivity.API.Controllers {
+    [ApiController]
+    [Route("api/repairs")]
+    public class RepairsController : ControllerBase {
+        private RepairService service = new RepairService();
+
+        [HttpPost("create-repair")]
+        public void AddRepair([FromBody] AddRepairBindingModel repair) {
+            service.UploadRepair("Value", repair);
+        }
+    }
+}
