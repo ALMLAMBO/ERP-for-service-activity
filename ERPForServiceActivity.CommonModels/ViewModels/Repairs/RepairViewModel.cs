85d3e007670d50f8f4404426d97dcbd550f8e2fe
ALMLAMBO <aleksm007148@gmail.com>
2020-01-18 15:51:43 +0200

    Add view model for the repair

---
+++ ERPForServiceActivity.CommonModels/ViewModels/Repairs/RepairViewModel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace ERPForServiceActivity.CommonModels.ViewModels.Repairs {
+	public class RepairViewModel {
+		public int RepairId { get; set; }
+		
+		public string CustomerName { get; set; }
+		
+		public string CustomerAddress { get; set; }
+		
+		public string CustomerPhoneNumber { get; set; }
+		
+		public string DefectByCustomer { get; set; }
+		
+		public bool GoingToAddress { get; set; }
+		
+		public bool InWarranty { get; set; }
+		
+		public string ApplianceBrand { get; set; }
+		
+		public string ApplianceType { get; set; }
+		
+		public string ApplianceModel { get; set; }
+		
+		public string ApplianceSerialNumber { get; set; }
+		
+		public string ApplianceProductCodeOrImei { get; set; }
+		
+		public string ApplianceEquipment { get; set; }
+		
+		public string BoughtFrom { get; set; }
+		
+		public string WarrantyCardNumber { get; set; }
+		
+		public int WarrantyPeriod { get; set; }
+
+		public DateTime BoughtAt { get; set; }
+
+		public string AdditionalInformation { get; set; }
+	}
+}
