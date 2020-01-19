c5f95dc83139ae5b27a0c2df01947bdad949127a
ALMLAMBO <aleksm007148@gmail.com>
2020-01-19 16:13:53 +0200

    Add method for getting all repairs

--- ERPForServiceActivity.Services/RepairService.cs
+++ ERPForServiceActivity.Services/RepairService.cs
@@ -1,9 +1,13 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Collections.Generic;
 using Google.Cloud.Firestore;
 using ERPForServiceActivity.Data;
 using ERPForServiceActivity.Models.Repairs;
 using ERPForServiceActivity.Services.Interfaces;
 using ERPForServiceActivity.CommonModels.BindingModels.Repairs;
+using ERPForServiceActivity.CommonModels.ViewModels.Repairs;
 
 namespace ERPForServiceActivity.Services {
 	public class RepairService : IRepairInterface {
@@ -85,6 +89,51 @@ namespace ERPForServiceActivity.Services {
 			}
 
 			return id;
+		}
+
+		public async Task<List<RepairViewModel>> GetAllRepairs(string serviceName) {
+			ConnectionConfig config = new ConnectionConfig();
+			FirestoreDb db = config.GetFirestoreDb();
+			List<RepairViewModel> repairs = new List<RepairViewModel>();
+
+			Query query = db.Collection("service-repairs")
+				.Document("kk5M3Pl4Zvme6db7kn7p")
+				.Collection("repairs");
+
+			QuerySnapshot snapshot = await query.GetSnapshotAsync();
+
+			foreach (DocumentSnapshot ds in snapshot.Documents) {
+				if(ds.Exists) {
+					RepairViewModel model = new RepairViewModel() {
+						RepairId = ds.GetValue<int>("RepairId"),
+						RepairStatus = ds.GetValue<string>("RepairStatus"),
+						CreatedAt = ds.GetValue<DateTime>("CreatedAt"),
+						CustomerName = ds.GetValue<string>("CustomerName"),
+						CustomerAddress = ds.GetValue<string>("CustomerAddress"),
+						CustomerPhoneNumber = ds.GetValue<string>("CustomerPhoneNumber"),
+						DefectByCustomer = ds.GetValue<string>("DefectByCustomer"),
+						GoingToAddress = ds.GetValue<bool>("GoingToAddress"),
+						InWarranty = ds.GetValue<bool>("InWarranty"),
+						ApplianceBrand = ds.GetValue<string>("ApplianceBrand"),
+						ApplianceType = ds.GetValue<string>("ApplianceType"),
+						ApplianceModel = ds.GetValue<string>("ApplianceModel"),
+						ApplianceSerialNumber = ds.GetValue<string>("ApplianceSerialNumber"),
+						ApplianceProductCodeOrImei = ds.GetValue<string>("ApplianceProductCodeOrImei"),
+						ApplianceEquipment = ds.GetValue<string>("ApplianceEquipment"),
+						BoughtFrom = ds.GetValue<string>("BoughtFrom"),
+						WarrantyCardNumber = ds.GetValue<string>("WarrantyCardNumber"),
+						WarrantyPeriod = ds.GetValue<int>("WarrantyPeriod"),
+						BoughtAt = ds.GetValue<DateTime>("BoughtAt"),
+						AdditionalInformation = ds.GetValue<string>("AdditionalInformation")
+					};
+
+					repairs.Add(model);
+				}
+			}
+
+			return repairs
+				.OrderBy(x => x.RepairId)
+				.ToList();
 		}
 	}
 }
