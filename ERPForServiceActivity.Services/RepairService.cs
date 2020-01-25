747795d84efa7861485795a015d54ea5e974d3fa
ALMLAMBO <aleksm007148@gmail.com>
2020-01-25 22:08:32 +0200

    Add methods for getting document id and returning repair view model

--- ERPForServiceActivity.Services/RepairService.cs
+++ ERPForServiceActivity.Services/RepairService.cs
@@ -18,20 +18,7 @@ namespace ERPForServiceActivity.Services {
 			int repairId = GetLastId(serviceName).Result;
 			++repairId;
 
-			CollectionReference colRef = db
-				.Collection("service-repairs");
-
-			Query query = colRef
-				.WhereEqualTo("ServiceName", serviceName);
-
-			string docId = string.Empty;
-			QuerySnapshot snapshot = await query.GetSnapshotAsync();
-
-			foreach (DocumentSnapshot ds in snapshot.Documents) {
-				if(ds.Exists) {
-					docId = ds.Id;
-				}
-			}
+			string docId = GetDocumentId("service-repairs", serviceName).Result;
 
 			CollectionReference repairsColRef = db
 				.Collection("service-repairs")
@@ -48,20 +35,32 @@ namespace ERPForServiceActivity.Services {
 			});
 		}
 
-		private async void UpdateRepairId(string serviceName, int id) {
+		private async Task<string> GetDocumentId(string collectionName,
+			string serviceName) {
+
 			FirestoreDb db = connection.GetFirestoreDb();
-			Query query = db
-				.Collection("last-id-repairs")
+
+			CollectionReference colRef = db
+				.Collection(collectionName);
+
+			Query query = colRef
 				.WhereEqualTo("ServiceName", serviceName);
 
-			QuerySnapshot qs = await query.GetSnapshotAsync();
 			string docId = string.Empty;
+			QuerySnapshot snapshot = await query.GetSnapshotAsync();
 
-			foreach (DocumentSnapshot ds in qs.Documents) {
-				if(ds.Exists) {
+			foreach (DocumentSnapshot ds in snapshot.Documents) {
+				if (ds.Exists) {
 					docId = ds.Id;
 				}
 			}
+
+			return docId;
+		}
+
+		private async void UpdateRepairId(string serviceName, int id) {
+			FirestoreDb db = connection.GetFirestoreDb();
+			string docId = GetDocumentId("last-id-repairs", serviceName).Result;
 
 			DocumentReference docRef = db
 				.Collection("last-id-repairs")
@@ -91,41 +90,47 @@ namespace ERPForServiceActivity.Services {
 			return id;
 		}
 
+		private RepairViewModel NewModel(DocumentSnapshot ds) {
+			return new RepairViewModel() {
+				RepairId = ds.GetValue<int>("RepairId"),
+				RepairStatus = ds.GetValue<string>("RepairStatus"),
+				CreatedAt = ds.GetValue<DateTime>("CreatedAt"),
+				CustomerName = ds.GetValue<string>("CustomerName"),
+				CustomerAddress = ds.GetValue<string>("CustomerAddress"),
+				CustomerPhoneNumber = ds.GetValue<string>("CustomerPhoneNumber"),
+				DefectByCustomer = ds.GetValue<string>("DefectByCustomer"),
+				GoingToAddress = ds.GetValue<bool>("GoingToAddress"),
+				InWarranty = ds.GetValue<bool>("InWarranty"),
+				ApplianceBrand = ds.GetValue<string>("ApplianceBrand"),
+				ApplianceType = ds.GetValue<string>("ApplianceType"),
+				ApplianceModel = ds.GetValue<string>("ApplianceModel"),
+				ApplianceSerialNumber = ds.GetValue<string>("ApplianceSerialNumber"),
+				ApplianceProductCodeOrImei = ds.GetValue<string>("ApplianceProductCodeOrImei"),
+				ApplianceEquipment = ds.GetValue<string>("ApplianceEquipment"),
+				BoughtFrom = ds.GetValue<string>("BoughtFrom"),
+				WarrantyCardNumber = ds.GetValue<string>("WarrantyCardNumber"),
+				WarrantyPeriod = ds.GetValue<int>("WarrantyPeriod"),
+				BoughtAt = ds.GetValue<DateTime>("BoughtAt"),
+				AdditionalInformation = ds.GetValue<string>("AdditionalInformation")
+			};
+		}
+
 		public async Task<List<RepairViewModel>> GetAllRepairs(string serviceName) {
 			ConnectionConfig config = new ConnectionConfig();
 			FirestoreDb db = config.GetFirestoreDb();
 			List<RepairViewModel> repairs = new List<RepairViewModel>();
 
+			string docId = GetDocumentId("service-repairs", serviceName).Result;
+
 			Query query = db.Collection("service-repairs")
-				.Document("kk5M3Pl4Zvme6db7kn7p")
+				.Document(docId)
 				.Collection("repairs");
 
 			QuerySnapshot snapshot = await query.GetSnapshotAsync();
 
 			foreach (DocumentSnapshot ds in snapshot.Documents) {
 				if(ds.Exists) {
-					RepairViewModel model = new RepairViewModel() {
-						RepairId = ds.GetValue<int>("RepairId"),
-						RepairStatus = ds.GetValue<string>("RepairStatus"),
-						CreatedAt = ds.GetValue<DateTime>("CreatedAt"),
-						CustomerName = ds.GetValue<string>("CustomerName"),
-						CustomerAddress = ds.GetValue<string>("CustomerAddress"),
-						CustomerPhoneNumber = ds.GetValue<string>("CustomerPhoneNumber"),
-						DefectByCustomer = ds.GetValue<string>("DefectByCustomer"),
-						GoingToAddress = ds.GetValue<bool>("GoingToAddress"),
-						InWarranty = ds.GetValue<bool>("InWarranty"),
-						ApplianceBrand = ds.GetValue<string>("ApplianceBrand"),
-						ApplianceType = ds.GetValue<string>("ApplianceType"),
-						ApplianceModel = ds.GetValue<string>("ApplianceModel"),
-						ApplianceSerialNumber = ds.GetValue<string>("ApplianceSerialNumber"),
-						ApplianceProductCodeOrImei = ds.GetValue<string>("ApplianceProductCodeOrImei"),
-						ApplianceEquipment = ds.GetValue<string>("ApplianceEquipment"),
-						BoughtFrom = ds.GetValue<string>("BoughtFrom"),
-						WarrantyCardNumber = ds.GetValue<string>("WarrantyCardNumber"),
-						WarrantyPeriod = ds.GetValue<int>("WarrantyPeriod"),
-						BoughtAt = ds.GetValue<DateTime>("BoughtAt"),
-						AdditionalInformation = ds.GetValue<string>("AdditionalInformation")
-					};
+					RepairViewModel model = NewModel(ds);
 
 					repairs.Add(model);
 				}
