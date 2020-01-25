d63dd0bb097995f3057191849b9e2cc2faa39890
ALMLAMBO <aleksm007148@gmail.com>
2020-01-25 22:06:20 +0200

    Remove class

--- ERPForServiceActivity.Common/IdClass.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace ERPForServiceActivity.Common {
-	public class IdClass {
-		public static int Id { get; set; } = 1;
-	}
-}
