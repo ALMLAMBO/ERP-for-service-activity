a4e57bc70d05a87c4ed53529f77373ebfb4bc819
ALMLAMBO <aleksm007148@gmail.com>
2020-01-28 16:49:46 +0200

    Add model for editing warehouse part

---
+++ ERPForServiceActivity.CommonModels/BindingModels/WarehouseParts/EditWarehousePartBindingModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace ERPForServiceActivity.CommonModels.BindingModels.WarehouseParts {
+	public class EditWarehousePartBindingModel {
+		[Required]
+		public int Availability { get; set; }
+	}
+}
