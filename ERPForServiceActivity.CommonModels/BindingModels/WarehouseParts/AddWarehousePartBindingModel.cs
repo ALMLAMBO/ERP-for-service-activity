b63831812ca63f3e78b117d033ffc4c17306b001
ALMLAMBO <aleksm007148@gmail.com>
2020-01-28 16:49:15 +0200

    Add model for adding parts in warehouse

---
+++ ERPForServiceActivity.CommonModels/BindingModels/WarehouseParts/AddWarehousePartBindingModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace ERPForServiceActivity.CommonModels.BindingModels.WarehouseParts {
+	public class AddWarehousePartBindingModel {
+		[Required]
+		public string PartNumber { get; set; }
+
+		[Required]
+		public int Availability { get; set; }
+
+		[Required]
+		public string Model { get; set; }
+
+		[Required]
+		public string Description { get; set; }
+
+		[Required]
+		public string Brand { get; set; }
+
+		public List<string> SubstituteParts { get; set; }
+
+		[Required]
+		public decimal Price { get; set; }
+
+		[Required]
+		public string Invoice { get; set; }
+
+		[Required]
+		public DateTime InvoiceDate { get; set; }
+
+		[Required]
+		public DateTime ReceivedDate { get; set; }
+	}
+}
