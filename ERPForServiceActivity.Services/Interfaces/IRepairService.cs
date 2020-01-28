15a723092dc1ec04b30ebdfb1d93bce40f6d4dd7
ALMLAMBO <aleksm007148@gmail.com>
2020-01-28 13:44:08 +0200

    Add method for getting all repairs

--- ERPForServiceActivity.Services/Interfaces/IRepairService.cs
+++ ERPForServiceActivity.Services/Interfaces/IRepairService.cs
@@ -1,7 +1,12 @@
-﻿using ERPForServiceActivity.CommonModels.BindingModels.Repairs;
+﻿using System.Threading.Tasks;
+using System.Collections.Generic;
+using ERPForServiceActivity.CommonModels.ViewModels.Repairs;
+using ERPForServiceActivity.CommonModels.BindingModels.Repairs;
 
 namespace ERPForServiceActivity.Services.Interfaces {
-	public interface IRepairInterface {
+	public interface IRepairService {
 		public void UploadRepair(string ServiceName, AddRepairBindingModel repair);
+
+		public Task<List<RepairViewModel>> GetAllRepairs(string serviceName);
 	}
 }
