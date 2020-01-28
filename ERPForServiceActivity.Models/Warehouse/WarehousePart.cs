4255e55f52fd8a1ba12a2301c34a9c28ab8d514f
ALMLAMBO <aleksm007148@gmail.com>
2020-01-28 16:51:04 +0200

    Add model for warehouse part which communicates with database

---
+++ ERPForServiceActivity.Models/Warehouse/WarehousePart.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+using Google.Cloud.Firestore;
+
+namespace ERPForServiceActivity.Models.Warehouse {
+	[FirestoreData]
+	public class WarehousePart {
+		public string Id { get; set; }
+
+		[Required]
+		[FirestoreProperty]
+		public string PartNumber { get; set; }
+
+		[Required]
+		[FirestoreProperty]
+		public int Availability { get; set; }
+
+		[Required]
+		[FirestoreProperty]
+		public string Model { get; set; }
+
+		[Required]
+		[FirestoreProperty]
+		public string Description { get; set; }
+
+		[Required]
+		[FirestoreProperty]
+		public string Brand { get; set; }
+
+		[FirestoreProperty]
+		public List<string> SubstituteParts { get; set; }
+
+		[Required]
+		[FirestoreProperty]
+		public decimal Price { get; set; }
+
+		[Required]
+		[FirestoreProperty]
+		public string Invoice { get; set; }
+
+		[Required]
+		[FirestoreProperty]
+		public DateTime InvoiceDate { get; set; }
+
+		[Required]
+		[FirestoreProperty]
+		public DateTime ReceivedDate { get; set; }
+	}
+}
