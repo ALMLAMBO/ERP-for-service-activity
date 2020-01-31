0e86a95985d4cb896a219d71da9d16587737522e
ALMLAMBO <aleksm007148@gmail.com>
2020-01-31 21:50:11 +0200

    Add controller for warehouse parts

---
+++ ERPForServiceActivity.API/Controllers/WarehousePartController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ERPForServiceActivity.Services;
+using ERPForServiceActivity.CommonModels.ViewModels.Warehouse;
+using ERPForServiceActivity.CommonModels.BindingModels.WarehouseParts;
+
+namespace ERPForServiceActivity.API.Controllers {
+    [Route("api/warehouse-parts")]
+    [ApiController]
+    public class WarehousePartController : ControllerBase {
+        private WarehousePartService service = 
+            new WarehousePartService();
+
+        [HttpGet("get-parts/{serviceName}")]
+        public async Task<List<WarehousePartViewModel>> 
+            GetWarehouseParts(string serviceName) {
+
+            return await service.GetAllParts(serviceName);
+        }
+
+        [HttpPost("add-part/{serviceName}")]
+        public void AddPart(
+            [FromBody] AddWarehousePartBindingModel newPart, 
+            string serviceName) {
+
+            service.AddWarehousePart(newPart, serviceName);
+        }
+    }
+}
