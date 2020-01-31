fdfea528ad205aa0fb8b9e13a375b9133fa44736
ALMLAMBO <aleksm007148@gmail.com>
2020-01-31 21:49:12 +0200

    Add interface for warehouse parts

---
+++ ERPForServiceActivity.Services/Interfaces/IWarehousePartService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using ERPForServiceActivity.CommonModels.ViewModels.Warehouse;
+using ERPForServiceActivity.CommonModels.BindingModels.WarehouseParts;
+
+namespace ERPForServiceActivity.Services.Interfaces {
+	public interface IWarehousePartService {
+		public void AddWarehousePart(AddWarehousePartBindingModel model,
+			string serviceName);
+
+		public Task<List<WarehousePartViewModel>> GetAllParts(string serviceName);
+	}
+}
