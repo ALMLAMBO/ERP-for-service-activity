44fbbdb9e9c0ef3911ac8823d40534b5ef80d6dc
ALMLAMBO <aleksm007148@gmail.com>
2020-02-03 14:16:30 +0200

    Add constructor and remove substitute modes and replace model type with list of strings

--- ERPForServiceActivity.Models/Warehouse/WarehousePart.cs
+++ ERPForServiceActivity.Models/Warehouse/WarehousePart.cs
@@ -1,12 +1,30 @@
 ﻿using System;
+using System.Text;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.Text;
 using Google.Cloud.Firestore;
+using ERPForServiceActivity.CommonModels.BindingModels.WarehouseParts;
 
 namespace ERPForServiceActivity.Models.Warehouse {
 	[FirestoreData]
 	public class WarehousePart {
+		public WarehousePart() {
+
+		}
+
+		public WarehousePart(AddWarehousePartBindingModel model) {
+			PartNumber = model.PartNumber;
+			Availability = model.Availability;
+			Brand = model.Brand;
+			Description = model.Description;
+			Model = model.Model;
+			Price = model.Price;
+			SubstituteParts = model.SubstituteParts;
+			Invoice = model.Invoice;
+			InvoiceDate = model.InvoiceDate.ToUniversalTime();
+			ReceivedDate = DateTime.UtcNow.ToUniversalTime();
+		}
+
 		public string Id { get; set; }
 
 		[Required]
@@ -19,7 +37,7 @@ namespace ERPForServiceActivity.Models.Warehouse {
 
 		[Required]
 		[FirestoreProperty]
-		public string Model { get; set; }
+		public List<string> Model { get; set; }
 
 		[Required]
 		[FirestoreProperty]
@@ -31,9 +49,6 @@ namespace ERPForServiceActivity.Models.Warehouse {
 
 		[FirestoreProperty]
 		public List<string> SubstituteParts { get; set; }
-
-		[FirestoreProperty]
-		public List<string> SubstituteModels { get; set; }
 
 		[Required]
 		[FirestoreProperty]
