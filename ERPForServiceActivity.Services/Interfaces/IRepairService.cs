3d0523fa36fad902e4310232f985c698db84e33e
ALMLAMBO <aleksm007148@gmail.com>
2020-02-05 15:09:16 +0200

    Add methods for processing images

--- ERPForServiceActivity.Services/Interfaces/IRepairService.cs
+++ ERPForServiceActivity.Services/Interfaces/IRepairService.cs
@@ -1,12 +1,28 @@
-﻿using System.Threading.Tasks;
+﻿using System.Drawing;
+using System.Threading.Tasks;
 using System.Collections.Generic;
+using MatBlazor;
 using ERPForServiceActivity.CommonModels.ViewModels.Repairs;
 using ERPForServiceActivity.CommonModels.BindingModels.Repairs;
 
 namespace ERPForServiceActivity.Services.Interfaces {
 	public interface IRepairService {
+		public Task<List<RepairViewModel>> GetAllRepairs(string serviceName);
+		
 		public void UploadRepair(string ServiceName, AddRepairBindingModel repair);
 
-		public Task<List<RepairViewModel>> GetAllRepairs(string serviceName);
+		public void SaveImageToTemp(IMatFileUploadEntry file);
+
+		public Bitmap ConvertToBitmap(IMatFileUploadEntry file);
+		
+		public void MoveImageToOriginalDirectory(Bitmap bitmap);
+
+		public void SaveMultipleImagesToTemp(IMatFileUploadEntry[] files);
+
+		public void MoveMultipleImagesToOriginalDirectory(Bitmap[] bitmaps);
+
+		public Bitmap[] ConvertMultipleToBitmap(IMatFileUploadEntry[] files);
+
+		public ResultFromOCRBindingModel GetData(IMatFileUploadEntry file);
 	}
 }
