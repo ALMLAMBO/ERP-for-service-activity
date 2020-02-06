702a2d1f27af51bdd2546ce46aa9c6d5a50c9df0
ALMLAMBO <aleksm007148@gmail.com>
2020-02-06 18:37:16 +0200

    Update methods

--- ERPForServiceActivity.Services/Interfaces/IRepairService.cs
+++ ERPForServiceActivity.Services/Interfaces/IRepairService.cs
@@ -9,23 +9,31 @@ namespace ERPForServiceActivity.Services.Interfaces {
 	public interface IRepairService {
 		public Task<List<RepairViewModel>> GetAllRepairs(string serviceName);
 		
-		public void UploadRepair(string ServiceName, AddRepairBindingModel repair);
-
-		public void SaveImageToTemp(IMatFileUploadEntry file);
+		public void UploadRepair(
+			string ServiceName, AddRepairBindingModel repair);
 
 		public Bitmap ConvertToBitmap(IMatFileUploadEntry file);
-		
-		public void MoveImageToOriginalDirectory(Bitmap bitmap);
 
-		public void SaveMultipleImagesToTemp(IMatFileUploadEntry[] files);
+		public void MoveImageToOriginalDirectory(
+			string path, int id);
 
-		public void MoveMultipleImagesToOriginalDirectory(Bitmap[] bitmaps);
+		public void SaveImageToTemp(
+			IMatFileUploadEntry file, string path);
 
-		public Bitmap[] ConvertMultipleToBitmap(IMatFileUploadEntry[] files);
+		public Bitmap[] ConvertMultipleToBitmap(
+			IMatFileUploadEntry[] files);
+
+		public void MoveMultipleImagesToOriginalDirectory(
+			string path, int id);
+
+		public void SaveMultipleImagesToTemp(
+			IMatFileUploadEntry[] files, string path);
 
 		public Task<int> GetLastId(string serviceName);
+		
+		public string GetImagePath(string path, int index);
 
 		public ResultFromOCRBindingModel GetData(
-			ResultFromOCRBindingModel model, IMatFileUploadEntry file);
+			ResultFromOCRBindingModel model);
 	}
 }
