37042b793520c5f17d489c9dd92c793e9e4c9e89
ALMLAMBO <aleksm007148@gmail.com>
2020-02-06 17:02:09 +0200

    Remove dictionary and add list for types of unit

--- ERPForServiceActivity.Common/BrandsInfo.cs
+++ ERPForServiceActivity.Common/BrandsInfo.cs
@@ -8,28 +8,8 @@ namespace ERPForServiceActivity.Common {
 			"LG", "Samsung", "Huawei", "Arielli", "Asus"
 		};
 
-		public static Dictionary<string, List<string>>
-			ModelsForBrand { get; set; } = 
-			new Dictionary<string, List<string>>() {
-				{"LG", new List<string>() {
-					"", "", ""
-				} },
-
-				{"Samsung", new List<string>() {
-					"", "", ""
-				} },
-
-				{"Asus", new List<string>() {
-					"", "", "" 
-				} },
-
-				{"Huawei", new List<string>() { 
-					"", "", "" 
-				} },
-
-				{"Arielli", new List<string>() { 
-					"", "", ""
-				} }
-			};
+		public static List<string> Types { get; set; } = new List<string>() {
+			"TV", "Washing Machine", "Oven", "Mobile Phone", "Refrigerator"
+		};
 	}
 }
