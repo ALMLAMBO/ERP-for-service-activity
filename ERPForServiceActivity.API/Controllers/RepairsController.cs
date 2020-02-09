655f1b0aff91588ecfa607893237b6e1a1687e52
ALMLAMBO <aleksm007148@gmail.com>
2020-02-09 15:18:53 +0200

    Add methods for getting last id and get data from image

--- ERPForServiceActivity.API/Controllers/RepairsController.cs
+++ ERPForServiceActivity.API/Controllers/RepairsController.cs
@@ -1,12 +1,13 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Collections.Generic;
+using MatBlazor;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Http;
-using ERPForServiceActivity.CommonModels.BindingModels.Repairs;
 using ERPForServiceActivity.Services;
 using ERPForServiceActivity.CommonModels.ViewModels.Repairs;
+using ERPForServiceActivity.CommonModels.BindingModels.Repairs;
 
 namespace ERPForServiceActivity.API.Controllers {
     [ApiController]
@@ -17,11 +18,37 @@ namespace ERPForServiceActivity.API.Controllers {
         [HttpPost("create-repair")]
         public void AddRepair([FromBody] AddRepairBindingModel repair) {
             service.UploadRepair("Value", repair);
+
+            //service.MoveImageToOriginalDirectory(
+            //    service.ConvertToBitmap(repair.ModelSNImage));
+            //
+            //service.MoveMultipleImagesToOriginalDirectory(
+            //    service.ConvertMultipleToBitmap(repair.OtherImages));
         }
 
         [HttpGet("get-repairs/{serviceName}")]
         public async Task<List<RepairViewModel>> GetAllRepairs(string serviceName) {
             return await service.GetAllRepairs(serviceName);
+        }
+
+        [HttpGet("get-last-id/{serviceName}")]
+        public async Task<int> GetLastId(string serviceName) {
+            return await Task.Run(() => {
+                return service.GetLastId(serviceName).Result;
+            });
+        }
+
+        [HttpPost("get-data")]
+        public ResultFromOCRBindingModel GetData(
+            [FromBody] AddRepairBindingModel model) {
+
+            ResultFromOCRBindingModel data =
+                new ResultFromOCRBindingModel() {
+                    ApplianceBrand = model.ApplianceBrand,
+                    ApplianceType = model.ApplianceType
+                };
+
+            return service.GetData(data);
         }
     }
 }
