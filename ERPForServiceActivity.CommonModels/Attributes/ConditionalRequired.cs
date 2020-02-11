44b85177dd819ea19b07c90a846178e419824de1
ALMLAMBO <aleksm007148@gmail.com>
2020-02-11 17:38:31 +0200

    Add annotation

---
+++ ERPForServiceActivity.CommonModels/Attributes/ConditionalRequired.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using ERPForServiceActivity.CommonModels.BindingModels.Repairs;
+
+namespace ERPForServiceActivity.CommonModels.Attributes {
+	public class ConditionalRequired : 
+		ValidationAttribute {
+
+		protected override ValidationResult IsValid(
+			object value, 
+			ValidationContext validationContext) {
+			
+			AddRepairBindingModel repair = 
+				(AddRepairBindingModel)
+				validationContext.ObjectInstance;
+		
+			if(repair.ApplianceType == "Mobile Phone") {
+				return new ValidationResult("IMEI is required");
+			}
+
+			string type = repair.ApplianceType;
+			return !type.Equals("Mobile Phone") ? 
+				new ValidationResult("Model and S/N are required") : 
+				ValidationResult.Success;
+		}
+	}
+}
