beb03ba5579277d7e6faab6ee223bdb62eccb8d0
ALMLAMBO <aleksm007148@gmail.com>
2020-02-20 22:44:17 +0200

    Use reflections for converting object and multithreaded execution for converting all result from the db

--- ERPForServiceActivity.Services/RepairService.cs
+++ ERPForServiceActivity.Services/RepairService.cs
@@ -98,36 +98,29 @@ namespace ERPForServiceActivity.Services {
 		}
 
 		private RepairViewModel NewModel(DocumentSnapshot ds) {
-			return new RepairViewModel() {
-				RepairId = ds.GetValue<int>("RepairId"),
-				RepairStatus = ds.GetValue<string>("RepairStatus"),
-				CreatedAt = ds.GetValue<DateTime>("CreatedAt"),
-				CustomerName = ds.GetValue<string>("CustomerName"),
-				CustomerAddress = ds.GetValue<string>("CustomerAddress"),
-				CustomerPhoneNumber = ds.GetValue<string>("CustomerPhoneNumber"),
-				DefectByCustomer = ds.GetValue<string>("DefectByCustomer"),
-				GoingToAddress = ds.GetValue<bool>("GoingToAddress"),
-				InWarranty = ds.GetValue<bool>("InWarranty"),
-				ApplianceBrand = ds.GetValue<string>("ApplianceBrand"),
-				ApplianceType = ds.GetValue<string>("ApplianceType"),
-				ApplianceModel = ds.GetValue<string>("ApplianceModel"),
-				ApplianceSerialNumber = ds.GetValue<string>("ApplianceSerialNumber"),
-				ApplianceProductCodeOrImei = ds.GetValue<string>("ApplianceProductCodeOrImei"),
-				ApplianceEquipment = ds.GetValue<string>("ApplianceEquipment"),
-				BoughtFrom = ds.GetValue<string>("BoughtFrom"),
-				WarrantyCardNumber = ds.GetValue<string>("WarrantyCardNumber"),
-				WarrantyPeriod = ds.GetValue<int>("WarrantyPeriod"),
-				BoughtAt = ds.GetValue<DateTime>("BoughtAt"),
-				AdditionalInformation = ds.GetValue<string>("AdditionalInformation")
-			};
+			RepairViewModel result = new RepairViewModel();
+
+			ds.ToDictionary()
+				.ToList()
+				.ForEach(keyVal => {
+					result
+						.GetType()
+						.GetProperty(keyVal.Key)
+						.SetValue(result, keyVal.Value);
+				});
+
+			return result;
 		}
 
-		public async Task<List<RepairViewModel>> GetAllRepairs(string serviceName) {
+		public async Task<List<RepairViewModel>>
+			GetAllRepairs(string serviceName) {
+			
 			ConnectionConfig config = new ConnectionConfig();
 			FirestoreDb db = config.GetFirestoreDb();
 			List<RepairViewModel> repairs = new List<RepairViewModel>();
 
-			string docId = GetDocumentId("service-repairs", serviceName).Result;
+			string docId = GetDocumentId(
+				"service-repairs", serviceName).Result;
 
 			Query query = db.Collection("service-repairs")
 				.Document(docId)
@@ -135,57 +128,17 @@ namespace ERPForServiceActivity.Services {
 
 			QuerySnapshot snapshot = await query.GetSnapshotAsync();
 
-			foreach (DocumentSnapshot ds in snapshot.Documents) {
-				if(ds.Exists) {
+			Parallel.ForEach(snapshot.Documents, ds => {
+				if (ds.Exists) {
 					RepairViewModel model = NewModel(ds);
 
 					repairs.Add(model);
 				}
-			}
+			});
 
 			return repairs
 				.OrderBy(x => x.RepairId)
 				.ToList();
-		}
-
-		public void SaveImageToTemp(IMatFileUploadEntry file) {
-			Bitmap bitmap = ConvertToBitmap(file);
-
-			bitmap.Save(CommonSecurityConstants
-				.PathToTempFolder + @$"\SN\{file.Name
-					.ToLower().Replace(" ", "-")}.jpeg",
-				ImageFormat.Jpeg);
-		}
-
-		public Bitmap ConvertToBitmap(IMatFileUploadEntry file) {
-			MemoryStream stream = new MemoryStream();
-			file.WriteToStreamAsync(stream);
-
-			return new Bitmap(stream);
-		}
-
-		public void MoveImageToOriginalDirectory(Bitmap bitmap) {
-			
-		}
-
-		public void SaveMultipleImagesToTemp(IMatFileUploadEntry[] files) {
-			foreach (IMatFileUploadEntry file in files) {
-				SaveImageToTemp(file);
-			}
-		}
-
-		public void MoveMultipleImagesToOriginalDirectory(Bitmap[] bitmaps) {
-			
-		}
-
-		public Bitmap[] ConvertMultipleToBitmap(IMatFileUploadEntry[] files) {
-			Bitmap[] bitmaps = new Bitmap[files.Length];
-
-			for (int i = 0; i < files.Length; i++) {
-				bitmaps[i] = ConvertToBitmap(files[i]);
-			}
-
-			return bitmaps;
 		}
 
 		public ResultFromOCRBindingModel GetData(
