7ab1948f29aec377a19da41db8eb44518b41b38a
ALMLAMBO <aleksm007148@gmail.com>
2020-02-22 22:48:52 +0200

    Add class connected to the form for adding parts to warehouse

---
+++ Second-version/ERPForServiceActivity/ERPForServiceActivity.CommonModels/BindingModels/WarehouseParts/AddWarehousePartBindingModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace ERPForServiceActivity.CommonModels.BindingModels.WarehouseParts {
+	public class AddWarehousePartBindingModel {
+		[Required]
+		public string PartNumber { get; set; }
+
+		[Required]
+		public int Availability { get; set; }
+
+		[Required]
+		public List<string> Model { get; set; } = new List<string>();
+
+		[Required]
+		public string Description { get; set; }
+
+		[Required]
+		public string Brand { get; set; }
+
+		public List<string> SubstituteParts { get; set; } = new List<string>();
+
+		[Required]
+		public double Price { get; set; }
+
+		[Required]
+		public string Invoice { get; set; }
+
+		[Required]
+		public DateTime InvoiceDate { get; set; }
+
+		[Required]
+		public DateTime ReceivedDate { get; set; }
+	}
+}
