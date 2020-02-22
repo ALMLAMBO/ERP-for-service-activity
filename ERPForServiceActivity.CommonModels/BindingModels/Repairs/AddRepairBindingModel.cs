a093b806c48da76e28dd9ebde3b01bc23c76373d
ALMLAMBO <aleksm007148@gmail.com>
2020-02-22 12:12:56 +0200

    Remove using statement for conditional required attribute

--- ERPForServiceActivity.CommonModels/BindingModels/Repairs/AddRepairBindingModel.cs
+++ ERPForServiceActivity.CommonModels/BindingModels/Repairs/AddRepairBindingModel.cs
@@ -4,7 +4,6 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using MatBlazor;
 using BlazorInputFile;
-using ERPForServiceActivity.CommonModels.Attributes;
 
 namespace ERPForServiceActivity.CommonModels.BindingModels.Repairs {
 	public class AddRepairBindingModel {
