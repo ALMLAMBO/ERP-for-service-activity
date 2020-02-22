80e2c2f0cceb963f1a2166bcd83348c8b827525c
ALMLAMBO <aleksm007148@gmail.com>
2020-02-22 12:05:48 +0200

    Remove all methods for directories and converting to bitmap

--- ERPForServiceActivity.Services/Interfaces/IRepairService.cs
+++ ERPForServiceActivity.Services/Interfaces/IRepairService.cs
@@ -12,28 +12,9 @@ namespace ERPForServiceActivity.Services.Interfaces {
 		public void UploadRepair(
 			string ServiceName, AddRepairBindingModel repair);
 
-		public Bitmap ConvertToBitmap(IMatFileUploadEntry file);
-
-		public void MoveImageToOriginalDirectory(
-			string path);
-
-		public void SaveImageToTemp(
-			IMatFileUploadEntry file, string path);
-
-		public Bitmap[] ConvertMultipleToBitmap(
-			IMatFileUploadEntry[] files);
-
-		public void MoveMultipleImagesToOriginalDirectory(
-			string path);
-
-		public void SaveMultipleImagesToTemp(
-			IMatFileUploadEntry[] files, string path);
-
 		public Task<int> GetLastId(string serviceName);
-		
-		public string GetImagePath(string path, int index);
-
+	
 		public ResultFromOCRBindingModel GetData(
-			ResultFromOCRBindingModel model);
+			ResultFromOCRBindingModel model, IMatFileUploadEntry file);
 	}
 }
