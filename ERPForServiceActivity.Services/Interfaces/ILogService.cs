f03ce8805d567d223d2bfb9a718eda29da841488
ALMLAMBO <aleksm007148@gmail.com>
2020-02-22 20:30:00 +0200

    Add interface for making logs

---
+++ ERPForServiceActivity.Services/Interfaces/ILogService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using ERPForServiceActivity.Models.Repairs;
+
+namespace ERPForServiceActivity.Services.Interfaces {
+	public interface ILogService {
+		Task UploadLog(RepairLog log);
+
+		Task<List<RepairLog>> GetLogsForRepair(int id);
+	}
+}
