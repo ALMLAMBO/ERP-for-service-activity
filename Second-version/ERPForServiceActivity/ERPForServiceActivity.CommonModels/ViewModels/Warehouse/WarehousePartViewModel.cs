bab7dda83c39fbe459b96f8a5fc73b99b2cdb74a
ALMLAMBO <aleksm007148@gmail.com>
2020-02-22 22:50:51 +0200

    Add view model for the warehouse part

---
+++ Second-version/ERPForServiceActivity/ERPForServiceActivity.CommonModels/ViewModels/Warehouse/WarehousePartViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using ERPForServiceActivity.Common;
+
+namespace ERPForServiceActivity.CommonModels.ViewModels.Warehouse {
+	public class WarehousePartViewModel {
+
+		public string PartNumber { get; set; }
+		
+		public int Availability { get; set; }
+
+		public List<string> Model { get; set; } = new List<string>();
+		
+		public string Description { get; set; }
+		
+		public string Brand { get; set; }
+		
+		public List<string> SubstituteParts { get; set; }
+
+		public double Price { get; set; }
+		
+		public string Invoice { get; set; }
+		
+		public DateTime InvoiceDate { get; set; }
+		
+		public DateTime ReceivedDate { get; set; }
+	}
+}
