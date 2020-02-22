bb72d3d55a191e82be764db12d8b37adc474ee3a
ALMLAMBO <aleksm007148@gmail.com>
2020-02-22 22:54:44 +0200

    Add interface for managing repairs

---
+++ Second-version/ERPForServiceActivity/ERPForServiceActivity.Services/Interfaces/IRepairService.cs
@@ -0,0 +1,23 @@
+﻿using System.Drawing;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using MatBlazor;
+using ERPForServiceActivity.Models.Repairs;
+using ERPForServiceActivity.CommonModels.ViewModels.Repairs;
+using ERPForServiceActivity.CommonModels.BindingModels.Repairs;
+
+namespace ERPForServiceActivity.Services.Interfaces {
+	public interface IRepairService {
+		public Task<List<RepairViewModel>> GetAllRepairs(string serviceName);
+		
+		public void UploadRepair(
+			string ServiceName, AddRepairBindingModel repair);
+
+		public void UploadLog(RepairLog log);
+
+		public Task<int> GetLastId(string serviceName);
+	
+		public ResultFromOCRBindingModel GetData(
+			ResultFromOCRBindingModel model, IMatFileUploadEntry file);
+	}
+}
