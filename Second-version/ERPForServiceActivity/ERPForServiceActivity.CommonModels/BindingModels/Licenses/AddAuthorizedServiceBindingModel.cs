0278633c2a0280496a958db25489c11bc9ed07d5
ALMLAMBO <aleksm007148@gmail.com>
2020-02-22 22:47:37 +0200

    Add class for adding authorized service that can use the program

---
+++ Second-version/ERPForServiceActivity/ERPForServiceActivity.CommonModels/BindingModels/Licenses/AddAuthorizedServiceBindingModel.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Google.Cloud.Firestore;
+using ERPForServiceActivity.Common;
+using ERPForServiceActivity.Security;
+
+namespace ERPForServiceActivity.CommonModels.BindingModels.Licenses {
+	public class AddAuthorizedServiceBindingModel {
+		public string Id { get; set; }
+		
+		[Required]
+		[DataType(DataType.Text)]
+		[MinLength(CommonConstants.MinLengthServiceName, 
+			ErrorMessage = CommonConstants.ErrorMessageWrongLength)]
+
+		public string AuthorizedService { get; set; }
+
+		[Required]
+		public string UserId { get; set; }
+
+		[Required]
+		[StringLength(CommonSecurityConstants.LicenseLength, 
+			ErrorMessage = CommonConstants.ErrorMessageIncorrectLicense)]
+
+		public string LicenseKey { get; set; }
+	}
+}
