b922533196da17ed7867cf43b2bb6c0fa0a97560
ALMLAMBO <aleksm007148@gmail.com>
2020-02-23 10:12:53 +0200

    Remove using statement

--- Second-version/ERPForServiceActivity/ERPForServiceActivity.CommonModels/BindingModels/Repairs/AddRepairBindingModel.cs
+++ Second-version/ERPForServiceActivity/ERPForServiceActivity.CommonModels/BindingModels/Repairs/AddRepairBindingModel.cs
@@ -3,7 +3,6 @@ using System.Text;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using MatBlazor;
-using BlazorInputFile;
 
 namespace ERPForServiceActivity.CommonModels.BindingModels.Repairs {
 	public class AddRepairBindingModel {
