ffb64c107984552772ae52974da8c169574fd1be
ALMLAMBO <aleksm007148@gmail.com>
2020-02-23 10:09:51 +0200

    Add controller for managing logs

---
+++ Second-version/ERPForServiceActivity/ERPForServiceActivity.API/Controllers/LogController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using ERPForServiceActivity.Services;
+using ERPForServiceActivity.Models.Repairs;
+
+namespace ERPForServiceActivity.API.Controllers {
+    [ApiController]
+    [Route("api/log")]
+    public class LogController : ControllerBase {
+        private LogService service = new LogService();
+
+        [HttpGet("get-all-logs/{id}")]
+        public async Task<List<RepairLog>> GetLogsForRepair(int id) {
+            return await service.GetLogsForRepair(id);
+        }
+
+        [HttpPost("add-log")]
+        public async void AddLog([FromBody] RepairLog log) {
+            await service.UploadLog(log);
+        }
+    }
+}
