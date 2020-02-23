1d73af2a9f8bcbe36783282d60a4d2637e6d57a8
ALMLAMBO <aleksm007148@gmail.com>
2020-02-23 15:57:29 +0200

    Add interface for getting location coordinates

---
+++ Second-version/ERPForServiceActivity/ERPForServiceActivity.Services/Interfaces/IMapsService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using ERPForServiceActivity.CommonModels.BindingModels.Maps;
+
+namespace ERPForServiceActivity.Services.Interfaces {
+	public interface IMapsService {
+		Task<MapsCoordinatesBindingModel> 
+			GetCoordinates(string address);
+	}
+}
