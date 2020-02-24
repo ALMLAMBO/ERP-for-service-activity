733e764c09a24bf830cd7575e4a27c04468a48c2
ALMLAMBO <aleksm007148@gmail.com>
2020-02-24 18:08:14 +0200

    Add interface for managing directories

---
+++ Second-version/ERPForServiceActivity/ERPForServiceActivity.Services/Interfaces/IDirManagementService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ERPForServiceActivity.Services.Interfaces {
+	public interface IDirManagementService {
+		public void CreateRootDir(bool cancelled);
+
+		public void CreateSubDirs();
+
+		public void SaveImage();
+
+		public void SaveMultipleImages();
+
+		public void MoveData();
+	}
+}
