4de923f64e21bd7ebfcbd90a0214486c2d3f0ddd
ALMLAMBO <aleksm007148@gmail.com>
2020-02-28 17:24:52 +0200

    Add constructor for setting document id

--- Second-version/ERPForServiceActivity/ERPForServiceActivity.Models/Repairs/RepairLog.cs
+++ Second-version/ERPForServiceActivity/ERPForServiceActivity.Models/Repairs/RepairLog.cs
@@ -6,14 +6,21 @@ using Google.Cloud.Firestore;
 namespace ERPForServiceActivity.Models.Repairs {
 	[FirestoreData]
 	public class RepairLog {
+		public RepairLog() {
+			DocId = RepairId.ToString();
+		}
+
+		[FirestoreDocumentId]
+		public string DocId { get; set; }
+
+		[FirestoreProperty]
+		public int RepairId { get; set; }
+
 		[FirestoreProperty]
 		public DateTime TimeOfEvent { get; set; }
 
 		[FirestoreProperty]
 		public string TypeOfEvent { get; set; }
-
-		[FirestoreProperty]
-		public int RepairId { get; set; }
 
 		[FirestoreProperty]
 		public string Status { get; set; }
