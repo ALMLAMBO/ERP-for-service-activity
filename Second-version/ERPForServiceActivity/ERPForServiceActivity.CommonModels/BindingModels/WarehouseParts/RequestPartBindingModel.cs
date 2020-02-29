d99fd0f0831f31c34c0900222b910373146018a7
ALMLAMBO <aleksm007148@gmail.com>
2020-02-29 14:31:46 +0200

    Add class for setting requested parts for repair

---
+++ Second-version/ERPForServiceActivity/ERPForServiceActivity.CommonModels/BindingModels/WarehouseParts/RequestPartBindingModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Google.Cloud.Firestore;
+
+namespace ERPForServiceActivity.CommonModels.BindingModels.WarehouseParts {
+	[FirestoreData]
+	public class RequestPartBindingModel {
+		[FirestoreProperty]
+		public int RepairId { get; set; }
+
+		[FirestoreProperty]
+		public Dictionary<string, int> PartsForRepair { get; set; }
+	}
+}
