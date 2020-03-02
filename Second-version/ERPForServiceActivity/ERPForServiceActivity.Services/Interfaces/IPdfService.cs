997bcfa5df839870962d062ad6a1dbe4955c2a43
ALMLAMBO <aleksm007148@gmail.com>
2020-03-02 17:09:44 +0200

    Add interface for generating pdf files

---
+++ Second-version/ERPForServiceActivity/ERPForServiceActivity.Services/Interfaces/IPdfService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using ERPForServiceActivity.Models.Repairs;
+
+namespace ERPForServiceActivity.Services.Interfaces {
+	public interface IPdfService {
+		public void CreatePdf(Repair reapir);
+	}
+}
