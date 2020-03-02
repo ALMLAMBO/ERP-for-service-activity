aa930024711d5ed3672e10c2ef0b91b943c8c7b7
ALMLAMBO <aleksm007148@gmail.com>
2020-03-02 17:22:28 +0200

    Add methods for parts for repairs, updating status, updating repair, getting parts and get last status

--- Second-version/ERPForServiceActivity/ERPForServiceActivity.Services/Interfaces/IRepairService.cs
+++ Second-version/ERPForServiceActivity/ERPForServiceActivity.Services/Interfaces/IRepairService.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using MatBlazor;
 using ERPForServiceActivity.Models.Repairs;
 using ERPForServiceActivity.CommonModels.ViewModels.Repairs;
+using ERPForServiceActivity.CommonModels.ViewModels.Warehouse;
 using ERPForServiceActivity.CommonModels.BindingModels.Repairs;
 
 namespace ERPForServiceActivity.Services.Interfaces {
@@ -19,5 +20,20 @@ namespace ERPForServiceActivity.Services.Interfaces {
 	
 		public Task<ResultFromOCRBindingModel> GetData(
 			/*ResultFromOCRBindingModel model, MatFileUploadEntry file*/);
+
+		public void UpdateStatus(string status, int id);
+
+		public void UpdateRepair(RepairViewModel model);
+
+		public void AddRequestedPartToRepair(
+			string partNumber, int id);
+
+		public void AddPartToRepair(string partNumber, int id);
+
+		public void SendPartToRepair(int id, string partNumber, int qnt);
+
+		public Dictionary<string, int> GetPartsForRepair(int id);
+
+		public Task<StatusResult> GetRepairStatus(string name, string reclaim);
 	}
 }
