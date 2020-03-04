89a59a3cc15fdfa1d8580673c0bd6c3783982efb
ALMLAMBO <aleksm007148@gmail.com>
2020-03-04 20:46:18 +0200

    Add models for checkup

---
+++ Second-version/ERPForServiceActivity/ERPForServiceActivity.CommonModels/ViewModels/Checkup/BaseCheckup.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ERPForServiceActivity.CommonModels.ViewModels.Checkup {
+	public class BaseCheckup {
+		public string ServiceName { get; set; }
+
+		public DateTime BeginDate { get; set; } = DateTime.UtcNow;
+		
+		public DateTime EndDate { get; set; } = DateTime.UtcNow; 
+	}
+}
---
+++ Second-version/ERPForServiceActivity/ERPForServiceActivity.CommonModels/ViewModels/Checkup/RepairStatusCheckupViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ERPForServiceActivity.CommonModels.ViewModels.Checkup {
+	public class RepairStatusCheckupViewModel : BaseCheckup {
+		public int RepairId { get; set; }
+
+		public string Status { get; set; }
+
+		public bool GoingToAddress { get; set; }
+
+		public string UnitModel { get; set; }
+
+		public string CustomerAddress { get; set; }
+
+		public string CustomerPhoneNumber { get; set; }
+	}
+}
