6a9a6e3b8538ebf5736c0f93b20b43e1a5f7c9d7
ALMLAMBO <aleksm007148@gmail.com>
2020-03-04 20:44:03 +0200

    Add interface for managing checkups

---
+++ Second-version/ERPForServiceActivity/ERPForServiceActivity.Services/Interfaces/ICheckupService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using ERPForServiceActivity.CommonModels.ViewModels.Checkup;
+
+namespace ERPForServiceActivity.Services.Interfaces {
+	public interface ICheckupService {
+		public Task<List<TechniciаnCheckupViewModel>> 
+			TechnicianCheckup(string serviceName, 
+			DateTime begin, DateTime end);
+
+		public Task<List<RepairStatusCheckupViewModel>> 
+			StatusCheckup(string status, 
+			DateTime begin, DateTime end);
+
+		public Task ExoprtTechniciansCheckupToExcel(
+			List<TechniciаnCheckupViewModel> results);
+
+		public Task ExportRepairStatusCheckupToExcel(
+			List<RepairStatusCheckupViewModel> results);
+	}
+}
