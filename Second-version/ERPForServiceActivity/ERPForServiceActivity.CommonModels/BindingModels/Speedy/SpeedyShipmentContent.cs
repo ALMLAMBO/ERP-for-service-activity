9892688ae9def7c680e990b2abfc1eb8074a4cb4
ALMLAMBO <aleksm007148@gmail.com>
2020-03-16 16:29:30 +0200

    Add class for speedy shipment content

---
+++ Second-version/ERPForServiceActivity/ERPForServiceActivity.CommonModels/BindingModels/Speedy/SpeedyShipmentContent.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+using System.Text.Json.Serialization;
+
+namespace ERPForServiceActivity.CommonModels.BindingModels.Speedy {
+	public class SpeedyShipmentContent {
+		[Required]
+		[JsonPropertyName("parcelsCount")]
+		public int ParcelsCount { get; set; }
+
+		[Required]
+		[JsonPropertyName("totalWeight")]
+		public double TotalWeight { get; set; }
+
+		[Required]
+		[JsonPropertyName("contents")]
+		public string Contents { get; set; }
+
+		[Required]
+		[JsonPropertyName("package")]
+		public string Package { get; set; }
+
+		[JsonPropertyName("documents")]
+		public bool Documents { get; set; }
+
+		[JsonPropertyName("palletized")]
+		public bool Palletized { get; set; }
+
+		[JsonPropertyName("parcels")]
+		public SpeedyShipmentParcel[] Parcels { get; set; }
+
+		[JsonPropertyName("pendingParcels")]
+		public bool PendingParcels { get; set; }
+	}
+}
