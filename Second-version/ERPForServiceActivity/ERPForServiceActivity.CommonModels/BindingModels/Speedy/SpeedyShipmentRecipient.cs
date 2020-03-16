c0eb13b6b2cb4652ad40ad814a2b914f4b6e6eaf
ALMLAMBO <aleksm007148@gmail.com>
2020-03-16 16:11:13 +0200

    Add class for recipient

---
+++ Second-version/ERPForServiceActivity/ERPForServiceActivity.CommonModels/BindingModels/Speedy/SpeedyShipmentRecipient.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json.Serialization;
+
+namespace ERPForServiceActivity.CommonModels.BindingModels.Speedy {
+	public class SpeedyShipmentRecipient : SpeedyShipmentSender {
+		[JsonPropertyName("objectName")]
+		public string ObjectName { get; set; }
+
+		[JsonPropertyName("pickupOfficeId")]
+		public int PickupOfficeId { get; set; }
+	}
+}
