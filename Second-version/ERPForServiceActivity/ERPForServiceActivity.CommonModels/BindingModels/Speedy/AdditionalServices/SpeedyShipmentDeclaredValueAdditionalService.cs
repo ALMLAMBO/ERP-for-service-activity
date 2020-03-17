9368680481a1cf8ba9f95afa72d659dba02d1d39
ALMLAMBO <aleksm007148@gmail.com>
2020-03-17 19:06:59 +0200

    Add class for declared value

---
+++ Second-version/ERPForServiceActivity/ERPForServiceActivity.CommonModels/BindingModels/Speedy/AdditionalServices/SpeedyShipmentDeclaredValueAdditionalService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json.Serialization;
+
+namespace ERPForServiceActivity.CommonModels.BindingModels.Speedy.AdditionalServices {
+	public class SpeedyShipmentDeclaredValueAdditionalService {
+		[JsonPropertyName("amount")]
+		public double Amount { get; set; }
+
+		[JsonPropertyName("fragile")]
+		public bool Fragile { get; set; }
+
+		[JsonPropertyName("ignoreIfNotApplicable")]
+		public bool IgnoreIfNotApplicable { get; set; }
+	}
+}
