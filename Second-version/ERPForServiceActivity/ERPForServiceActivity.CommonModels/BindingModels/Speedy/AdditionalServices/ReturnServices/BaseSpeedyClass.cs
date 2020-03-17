b648f36e7a891222d69c26845e5fac2be2daab71
ALMLAMBO <aleksm007148@gmail.com>
2020-03-17 19:07:25 +0200

    Add base class

---
+++ Second-version/ERPForServiceActivity/ERPForServiceActivity.CommonModels/BindingModels/Speedy/AdditionalServices/ReturnServices/BaseSpeedyClass.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json.Serialization;
+
+namespace ERPForServiceActivity.CommonModels.BindingModels.Speedy.AdditionalServices.ReturnServices {
+	public class BaseSpeedyClass {
+		[JsonPropertyName("enabled")]
+		public bool Enabled { get; set; }
+
+		[JsonPropertyName("returnToClientId")]
+		public long ReturnToClientId { get; set; }
+
+		[JsonPropertyName("returnToOfficeId")]
+		public int ReturnToOfficeId { get; set; }
+
+		[JsonPropertyName("thirdPartyPayer")]
+		public bool ThirdPartyPayer { get; set; }
+	}
+}
