b98039d227452867a59329ad013e8bc7307ab6a2
ALMLAMBO <aleksm007148@gmail.com>
2020-03-17 19:08:43 +0200

    Add class for returning receipt

---
+++ Second-version/ERPForServiceActivity/ERPForServiceActivity.CommonModels/BindingModels/Speedy/AdditionalServices/ReturnServices/SpeedyShipmentReturnReceiptAdditionalService.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ERPForServiceActivity.CommonModels.BindingModels.Speedy.AdditionalServices.ReturnServices {
+	public class SpeedyShipmentReturnReceiptAdditionalService : 
+		BaseSpeedyClass {
+	}
+}
