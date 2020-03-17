10a784df92dce820ff10b316097dcff73b6e5b24
ALMLAMBO <aleksm007148@gmail.com>
2020-03-17 19:06:29 +0200

    Add class for cod

---
+++ Second-version/ERPForServiceActivity/ERPForServiceActivity.CommonModels/BindingModels/Speedy/AdditionalServices/SpeedyShipmentCODAdditionalService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+using System.Text.Json.Serialization;
+
+namespace ERPForServiceActivity.CommonModels.BindingModels.Speedy.AdditionalServices {
+	public class SpeedyShipmentCODAdditionalService {
+		[Required]
+		[JsonPropertyName("amount")]
+		public double Amount { get; set; }
+
+		[JsonPropertyName("currencyCode")]
+		public string CurrencyCode { get; set; }
+
+		[JsonPropertyName("processingType")]
+		public string ProcessingType { get; set; } = "CASH";
+
+		[JsonPropertyName("payoutToThirdParty")]
+		public bool PayoutToThirdParty { get; set; }
+
+		[JsonPropertyName("payoutToLoggedClient")]
+		public bool PayoutToLoggedClient { get; set; }
+
+		[JsonPropertyName("includeShippingPrice")]
+		public bool IncludeShippingPrice { get; set; }
+	}
+}
