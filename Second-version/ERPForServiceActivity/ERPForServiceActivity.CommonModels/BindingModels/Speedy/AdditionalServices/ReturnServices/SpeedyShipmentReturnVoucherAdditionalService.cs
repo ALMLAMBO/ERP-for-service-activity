da6998224af10b1625979c2979903ca16e2514af
ALMLAMBO <aleksm007148@gmail.com>
2020-03-17 19:09:16 +0200

    Add class for voucher

---
+++ Second-version/ERPForServiceActivity/ERPForServiceActivity.CommonModels/BindingModels/Speedy/AdditionalServices/ReturnServices/SpeedyShipmentReturnVoucherAdditionalService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json.Serialization;
+
+namespace ERPForServiceActivity.CommonModels.BindingModels.Speedy.AdditionalServices.ReturnServices {
+	public class SpeedyShipmentReturnVoucherAdditionalService {
+		[JsonPropertyName("serviceId")]
+		public int ServiceId { get; set; }
+
+		[JsonPropertyName("payer")]
+		public string Payer { get; set; }
+	}
+}
