adce3ce755dbef9a7b79329d0b8aa417901e16ec
ALMLAMBO <aleksm007148@gmail.com>
2020-03-17 19:07:58 +0200

    Add class for return of documents

---
+++ Second-version/ERPForServiceActivity/ERPForServiceActivity.CommonModels/BindingModels/Speedy/AdditionalServices/ReturnServices/SpeedyShipmentRODAdditionalService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json.Serialization;
+
+namespace ERPForServiceActivity.CommonModels.BindingModels.Speedy.AdditionalServices.ReturnServices {
+	public class SpeedyShipmentRODAdditionalService : 
+		BaseSpeedyClass {
+
+		[JsonPropertyName("comment")]
+		public string Comment { get; set; }
+	}
+}
