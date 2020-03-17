55cd735de68187c002126485fb09801f30f8bb30
ALMLAMBO <aleksm007148@gmail.com>
2020-03-17 19:11:13 +0200

    Add class for swapping

---
+++ Second-version/ERPForServiceActivity/ERPForServiceActivity.CommonModels/BindingModels/Speedy/AdditionalServices/ReturnServices/SpeedyShipmentSWAPAdditionalService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json.Serialization;
+
+namespace ERPForServiceActivity.CommonModels.BindingModels.Speedy.AdditionalServices.ReturnServices {
+	public class SpeedyShipmentSWAPAdditionalService {
+		[JsonPropertyName("serviceId")]
+		public int ServiceId { get; set; }
+
+		[JsonPropertyName("parcelsCount")]
+		public int ParcelsCount { get; set; }
+
+		[JsonPropertyName("declaredValue")]
+		public double DeclaredValue { get; set; }
+
+		[JsonPropertyName("fragile")]
+		public bool Fragile { get; set; }
+
+		[JsonPropertyName("returnToClientId")]
+		public long ReturnToClientId { get; set; }
+
+		[JsonPropertyName("returnToOfficeId")]
+		public int ReturnToOfficeId { get; set; }
+	}
+}
