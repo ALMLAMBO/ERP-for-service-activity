957c626f90323c0c596220122c1dabcb6ee9edfd
ALMLAMBO <aleksm007148@gmail.com>
2020-03-17 19:04:16 +0200

    Add properties to the class

--- Second-version/ERPForServiceActivity/ERPForServiceActivity.CommonModels/BindingModels/Speedy/SpeedyShipmentAdditionalServices.cs
+++ Second-version/ERPForServiceActivity/ERPForServiceActivity.CommonModels/BindingModels/Speedy/SpeedyShipmentAdditionalServices.cs
@@ -1,8 +1,30 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Text;
+using System.Collections.Generic;
+using System.Text.Json.Serialization;
+using ERPForServiceActivity.CommonModels.BindingModels.Speedy.AdditionalServices;
 
 namespace ERPForServiceActivity.CommonModels.BindingModels.Speedy {
 	public class SpeedyShipmentAdditionalServices {
+		[JsonPropertyName("cod")]
+		public SpeedyShipmentCODAdditionalService Cod { get; set; }
+
+		[JsonPropertyName("obpd")]
+		public SpeedyShipmentOBPD Obpd { get; set; }
+
+		[JsonPropertyName("declaredValue")]
+		public SpeedyShipmentDeclaredValueAdditionalService DeclaredValue { get; set; }
+
+		[JsonPropertyName("fixedTimeDelivery")]
+		public int FixedTimeDelivery { get; set; }
+
+		[JsonPropertyName("returns")]
+		public SpeedyShipmentAdditionalServices Returns { get; set; }
+
+		[JsonPropertyName("specialDeliveryId")]
+		public int SpecialDeliveryId { get; set; }
+
+		[JsonPropertyName("deliveryToFloor")]
+		public int DeliveryToFloor { get; set; }
 	}
 }
