ce1bd3ea132c255cdf392c2aad74679b21925ee8
ALMLAMBO <aleksm007148@gmail.com>
2020-03-17 19:04:45 +0200

    Add class options before payment

---
+++ Second-version/ERPForServiceActivity/ERPForServiceActivity.CommonModels/BindingModels/Speedy/AdditionalServices/SpeedyShipmentOBPD.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json.Serialization;
+
+namespace ERPForServiceActivity.CommonModels.BindingModels.Speedy.AdditionalServices {
+	public class SpeedyShipmentOBPD {
+		[JsonPropertyName("option")]
+		public string Option { get; set; }
+
+		[JsonPropertyName("returnShipmentServiceId")]
+		public int ReturnShipmentServiceId { get; set; }
+
+		[JsonPropertyName("returnShipmentPayer")]
+		public string ReturnShipmentPayer { get; set; }
+	}
+}
