79bf4bf60ace26e0f6f3e3c379546412d41e1432
ALMLAMBO <aleksm007148@gmail.com>
2020-03-17 19:10:16 +0200

    Add helper class

---
+++ Second-version/ERPForServiceActivity/ERPForServiceActivity.CommonModels/BindingModels/Speedy/AdditionalServices/ReturnServices/SpeedyShipmentROPAdditionalServiceLine.cs
@@ -0,0 +1,11 @@
+﻿using System.Text.Json.Serialization;
+
+namespace ERPForServiceActivity.CommonModels.BindingModels.Speedy.AdditionalServices.ReturnServices {
+	public class SpeedyShipmentROPAdditionalServiceLine {
+		[JsonPropertyName("serviceId")]
+		public int ServiceId { get; set; }
+
+		[JsonPropertyName("parcelsCount")]
+		public int ParcelsCount { get; set; }
+	}
+}
