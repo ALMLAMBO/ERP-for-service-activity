67935a9ec9cf66d7c2c68ba81b45f24e91f0fd27
ALMLAMBO <aleksm007148@gmail.com>
2020-03-17 19:09:41 +0200

    Add class for return of pallets

---
+++ Second-version/ERPForServiceActivity/ERPForServiceActivity.CommonModels/BindingModels/Speedy/AdditionalServices/ReturnServices/SpeedyShipmentROPAdditionalService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json.Serialization;
+
+namespace ERPForServiceActivity.CommonModels.BindingModels.Speedy.AdditionalServices.ReturnServices {
+	public class SpeedyShipmentROPAdditionalService {
+		[JsonPropertyName("pallets")]
+		public List<SpeedyShipmentROPAdditionalServiceLine> Pallets { get; set; }
+
+		[JsonPropertyName("thirdPartyPayer")]
+		public bool ThirdPartyPayer { get; set; }
+	}
+}
