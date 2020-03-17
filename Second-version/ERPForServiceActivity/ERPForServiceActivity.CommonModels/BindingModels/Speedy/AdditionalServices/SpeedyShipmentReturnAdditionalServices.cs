508e2d8e0eac05cc66739ace34664b3f9212171f
ALMLAMBO <aleksm007148@gmail.com>
2020-03-17 19:05:41 +0200

    Add class for return services

---
+++ Second-version/ERPForServiceActivity/ERPForServiceActivity.CommonModels/BindingModels/Speedy/AdditionalServices/SpeedyShipmentReturnAdditionalServices.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json.Serialization;
+using ERPForServiceActivity.CommonModels.BindingModels.Speedy.AdditionalServices.ReturnServices;
+
+namespace ERPForServiceActivity.CommonModels.BindingModels.Speedy.AdditionalServices {
+	public class SpeedyShipmentReturnAdditionalServices {
+		[JsonPropertyName("rod")]
+		public SpeedyShipmentRODAdditionalService Rod { get; set; }
+
+		[JsonPropertyName("returnReceipt")]
+		public SpeedyShipmentReturnReceiptAdditionalService ReturnReceipt { get; set; }
+
+		[JsonPropertyName("swap")]
+		public SpeedyShipmentSWAPAdditionalService Swap { get; set; }
+
+		[JsonPropertyName("rop")]
+		public SpeedyShipmentROPAdditionalService Rop { get; set; }
+
+		[JsonPropertyName("returnVoucher")]
+		public SpeedyShipmentReturnVoucherAdditionalService ReturnVoucher { get; set; }
+	}
+}
