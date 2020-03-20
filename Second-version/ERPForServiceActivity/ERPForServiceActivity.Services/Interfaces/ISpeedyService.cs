1b99b6a5bb1d7e074d007f169c902baf42b5c8f9
ALMLAMBO <aleksm007148@gmail.com>
2020-03-20 15:31:29 +0200

    Add interface for speedy service

---
+++ Second-version/ERPForServiceActivity/ERPForServiceActivity.Services/Interfaces/ISpeedyService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using ERPForServiceActivity.CommonModels.ViewModels.Speedy;
+using ERPForServiceActivity.CommonModels.BindingModels.Speedy;
+
+namespace ERPForServiceActivity.Services.Interfaces {
+	public interface ISpeedyService {
+		public string
+			CreateConsignment(
+			AddSpeedyShipmentBindingModel model, int id);
+	}
+}
