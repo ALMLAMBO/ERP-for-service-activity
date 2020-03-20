fceb64834ce6f6ab4a51d798d812585239ab4411
ALMLAMBO <aleksm007148@gmail.com>
2020-03-20 15:32:33 +0200

    Add class for db

---
+++ Second-version/ERPForServiceActivity/ERPForServiceActivity.Models/Speedy/SpeedyShipment.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using ERPForServiceActivity.CommonModels.BindingModels;
+using ERPForServiceActivity.CommonModels.ViewModels.Speedy;
+
+namespace ERPForServiceActivity.Models.Speedy {
+	public class SpeedyShipment {
+		public int RepairId { get; set; }
+
+		public SpeedyShipmentViewModel Shipment { get; set; }
+	}
+}
