835e9263611c393c4435a3e5f1f4c92ebe6e5ef8
ALMLAMBO <aleksm007148@gmail.com>
2020-06-02 14:35:26 +0300

    Resolve conflicts

---
+++ Third-version/ERPForServiceActivity/ERPForServiceActivity.Services/Implementations/Repairs/RepairService.cs
@@ -0,0 +1,30 @@
+﻿using System.Threading.Tasks;
+using Google.Cloud.Firestore;
+using ERPForServiceActivity.Models.Repairs;
+using ERPForServiceActivity.Services.Interfaces.Repairs;
+using ERPForServiceActivity.CommonModels.BindingModels.Repairs;
+
+namespace ERPForServiceActivity.Services.Implementations.Repairs {
+	public class RepairService : BaseService, IRepairService {
+		public async Task<bool> UploadRepair(
+			AddRepairBindingModel model) {
+
+			Repair repair = Mapper.Map<Repair>(model);
+
+			CollectionReference collection = Database
+				.Collection("service-repairs");
+
+			bool result = await Database
+				.RunTransactionAsync(async t => {
+					Task<DocumentReference> resultFromAdding =
+						collection.AddAsync(repair);
+
+					await resultFromAdding;
+
+					return resultFromAdding.IsCompletedSuccessfully;
+				});
+
+			return result;
+		}
+	}
+}
