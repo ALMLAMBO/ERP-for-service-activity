40e087716cc40c812b2a07d362a5b59d42cea9d8
ALMLAMBO <aleksm007148@gmail.com>
2020-06-02 16:26:50 +0300

    Call extension methods

--- Third-version/ERPForServiceActivity/ERPForServiceActivity.App/Startup.cs
+++ Third-version/ERPForServiceActivity/ERPForServiceActivity.App/Startup.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using ERPForServiceActivity.App.ExtensionMethods;
 
 namespace ERPForServiceActivity.App {
 	public class Startup {
@@ -17,6 +18,11 @@ namespace ERPForServiceActivity.App {
 		public void ConfigureServices(IServiceCollection services) {
 			services.AddRazorPages();
 			services.AddServerSideBlazor();
+
+			services.AddServices();
+			services.ConfigureAuthentication();
+			services.ConfigureHttpClient();
+			services.ConfigureMatToast();
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
