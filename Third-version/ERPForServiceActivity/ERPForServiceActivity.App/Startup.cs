1dcf9d5377be0cb057e39e87bc6961f43eee2934
ALMLAMBO <aleksm007148@gmail.com>
2020-06-02 15:56:20 +0300

    Code cleanup

--- Third-version/ERPForServiceActivity/ERPForServiceActivity.App/Startup.cs
+++ Third-version/ERPForServiceActivity/ERPForServiceActivity.App/Startup.cs
@@ -1,15 +1,8 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using ERPForServiceActivity.App.Data;
 
 namespace ERPForServiceActivity.App {
 	public class Startup {
@@ -24,7 +17,6 @@ namespace ERPForServiceActivity.App {
 		public void ConfigureServices(IServiceCollection services) {
 			services.AddRazorPages();
 			services.AddServerSideBlazor();
-			services.AddSingleton<WeatherForecastService>();
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
