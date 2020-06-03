945ee354bd4e29a738d782951cfeaff2f218ed7c
ALMLAMBO <aleksm007148@gmail.com>
2020-06-03 17:24:37 +0300

    Add error messages

--- Third-version/ERPForServiceActivity/ERPForServiceActivity.Common/ErrorMessages.cs
+++ Third-version/ERPForServiceActivity/ERPForServiceActivity.Common/ErrorMessages.cs
@@ -1,10 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace ERPForServiceActivity.Common {
+﻿namespace ERPForServiceActivity.Common {
 	public class ErrorMessages {
 		public const string InvalidPhoneNumber = "Invalid phone number";
 		public const string TooLong = "This is too long";
+
+		public const string NameRequired = "Name is required";
+		public const string AddressRequired = "Address is required";
+		public const string PhoneRequired = "Phone is required";
+		public const string DefectByCustomerRequired = "Defect by customer is required";
+
+		public const string BrandRequired = "Unit brand is required";
+		public const string TypeRequired = "Unit type is required";
+
+		public const string ModelRequired = "Unit model is required";
+		public const string SerialNumberRequired = "Unit serial number is required";
+		public const string ProductCodeOrImeiRequired = 
+			"Unit product imei is required";
+
+		public const string EquipmentRequired = "Unit equipment is required";
+		public const string BoughtFromRequired = "Fill out from where is bought";
+		public const string WarrantyCardNumberRequired =
+			"Unit warranty card number is required";
 	}
 }
