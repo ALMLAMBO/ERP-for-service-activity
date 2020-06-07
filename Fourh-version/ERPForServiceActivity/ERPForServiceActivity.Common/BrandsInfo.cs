fddcc8f7b0dcb0c5145f7606022b7aa4ad1f54e1
ALMLAMBO <aleksm007148@gmail.com>
2020-06-07 23:45:19 +0300

    Add class for brands info

---
+++ Fourh-version/ERPForServiceActivity/ERPForServiceActivity.Common/BrandsInfo.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+namespace ERPForServiceActivity.Common {
+	public class BrandsInfo {
+		public static List<string> Brands {
+			get {
+				return new List<string>() {
+					"LG", "Samsung", "Huawei",
+					"Arielli", "Asus"
+				};
+			}
+		} 
+			
+
+		public static List<string> Types {
+			get {
+				return new List<string>() {
+					"TV", "Washing Machine", "Oven",
+					"Mobile Phone", "Refrigerator"
+				};
+			}
+		} 
+			
+	}
+}
