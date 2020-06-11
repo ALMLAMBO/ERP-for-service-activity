ec2472d15107ebf03252ff0fcd2e7b019cdab2ac
ALMLAMBO <aleksm007148@gmail.com>
2020-06-11 18:16:13 +0300

    Add class for db connection

---
+++ Fifth-version/ERPForServiceActivity/ERPForServiceActivity.Data/DatabaseConnection.cs
@@ -0,0 +1,26 @@
+﻿using System.Threading.Tasks;
+using Google.Cloud.Firestore;
+using Google.Apis.Auth.OAuth2;
+using ERPForServiceActivity.Security;
+
+namespace ERPForServiceActivity.Data {
+	public class DatabaseConnection {
+		private FirestoreDb database;
+
+		private async Task Setup() {
+			await GoogleCredential
+				.FromFileAsync(
+					CommonSecurityConstants.PathToFirestoreJson,
+					default);
+
+			database = await FirestoreDb
+				.CreateAsync(CommonSecurityConstants.ProjectId);
+		}
+
+		public async Task<FirestoreDb> GetFirestoreDb() {
+			await Setup();
+
+			return database;
+		}
+	}
+}
